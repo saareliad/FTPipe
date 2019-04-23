6fece5d8639ef5ae3344774f5d351462421f24c9
Or Borenstein <borenstein@campus.technion.ac.il>
2019-04-23 16:12:43 +0300

    added code for pipeline parallel (without testing)

---
+++ pipeline/pipeline_parallel.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+
+from pipeline.utils import prod_line
+
+
+class PipelineParallel(nn.Module):
+    """
+
+    """
+    def __init__(self, submodules, devices, mb_size):
+        super(PipelineParallel, self).__init__()
+
+        self.mb_size = mb_size
+        self.devices = devices
+        self.submodules = submodules
+
+    def forward(self, input: torch.Tensor):
+        input = input.view((-1, self.mb_size, *tuple(input.shape[1:])))
+
+        results = prod_line(
+            input, self.submodules,
+            last_ac=lambda x: x.to('cpu')
+        )
+
+        return torch.cat(results, dim=0)
+
+    def backward(self, loss_fn, results, targets):
+        num_samples = float(results.shape[0])
+        results = results.view((-1, self.mb_size, *tuple(results.shape[1:])))
+        targets = targets.view((-1, self.mb_size, *tuple(targets.shape[1:])))
+
+        losses = [loss_fn(res, tar) for res, tar in zip(results, targets)]
+        losses = [torch.sum(loss)/num_samples for loss in losses]
+
+        for loss in losses:
+            loss.backwards()
+
+        grads = [loss.grad for loss in losses[::-1]]
+
+        actions = [m.backward for m in self.submodules[::-1]]
+        prod_line(grads, actions, output_results=False)
