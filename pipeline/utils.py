b9bed39f0126d15d873de4f9aa0dda735d8079e0
Assaf-Haik-Barouch <Assaf98@gmail.com>
2019-04-23 14:27:49 +0200

    added utils file

---
+++ pipeline/utils.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+
+def conveyor_gen(inputs, queue):
+    for x in inputs:
+        yield x
+
+    while not all([x is None for x in queue]):
+        yield None
+
+
+def prod_line(inputs, actions, size, output_results=True, first_ac=None, last_ac=None):
+    assert size == len(actions)
+
+    outputs = deque([]) if output_results else None
+
+    actions = [lambda x: x if x is None else act(x) for act in actions]
+
+    # make sure that the first and last actions are identity if none were inputted
+    if first_ac is None:
+        first_ac = lambda x: x
+
+    if last_ac is None:
+        last_ac = lambda x: x
+    #
+    first_ac = lambda x: x if x is None else first_ac(x)
+    last_ac = lambda x: x if x is None else last_ac(x)
+
+    belt = deque([None for _ in range(size - 1)])
+
+    for x in conveyor_gen(inputs, belt):
+        belt.appendleft(first_ac(x))
+
+        for i, act in enumerate(actions):
+            belt[i] = act(belt[i])
+
+        last_val = last_ac(belt.pop())
+
+        if output_results and last_val is not None:
+            outputs.appendleft(last_val)
+
+    return outputs
+
