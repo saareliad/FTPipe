fe7ae1f289a282614067d3a0bf43250135133215
alondj <alondej@gmail.com>
2019-04-24 16:05:30 +0300

    time profiling for forward and backward

--- model_Inference/network_profiler.py
+++ model_Inference/network_profiler.py
@@ -4,51 +4,96 @@ import timeit
 
 
 class Wrapper(nn.Module):
-    def __init__(self, sub_module, idx):
+    def __init__(self, sub_module: nn.Module, idx):
         super(Wrapper, self).__init__()
         self.module = sub_module
         self.idx = idx
-        self.cost = None
-        self.in_nodes = []
+        self.forward_time = None
+        self.backward_time = None
 
-    def forward(self, data_in):
-        cost = 0
-        x, times = data_in
+        def time_hook(module, grad_in, grad_out):
+            self.backward_time = timeit.time.time()
+
+        self.module.register_backward_hook(time_hook)
+
+    def forward(self, x):
+        forward_time = 0
+        out = None
         if(x.is_cuda):
             # milliseconds
             torch.cuda.synchronize()
             start = torch.cuda.Event(enable_timing=True)
             end = torch.cuda.Event(enable_timing=True)
             start.record()
-            self.module(x)
+            out = self.module(x)
             end.record()
             torch.cuda.synchronize()
-
-            cost = (start.elapsed_time(end))
+            forward_time = 0
+            forward_time = (start.elapsed_time(end))
         else:
             # convert to milliseconds
-            cost = 1000*timeit.Timer(
-                lambda: self.module(x)).timeit(1)
-        self.cost = cost
-        return (self.module(x), times+[cost])
+            start = timeit.time.time()
+            out = self.module(x)
+            end = timeit.time.time()
+            forward_time = 1000*(end - start)
+
+        self.forward_time = forward_time
+
+        return out
 
 
 class NetProfiler(nn.Module):
-    def __init__(self, module):
+    def __init__(self, module, device="cuda"):
         super(NetProfiler, self).__init__()
         self.module = module
-        self.num_layers, self.layers_dict = self._wrap_individual_layers(
+        self.num_layers, self.layers = self._wrap_individual_layers(
             self.module, 0, {})
+        self.device = device
+        self._forward_times = None
+        self._backward_times = None
+        self._weights_sizes = 1
 
     def forward(self, x):
-        out, _ = self.module((x, []))
+        out = x.to(self.device)
+        self.to(self.device)
+        # warmup
+        a = torch.randn(1000, 2000).to(self.device)
+        b = torch.randn(2000, 1000).to(self.device)
+        a.mm(b)
+        if self.device == "cuda":
+            print("hello")
+            torch.cuda.synchronize()
+
+        out = self.module(out)
+        # gather forward times results
+        self._forward_times = [
+            layer.forward_time for layer in self.layers.values()]
+
         return out
+
+    def _gather_backward_times(self):
+        self._backward_times = [
+            layer.backward_time for layer in self.layers.values()]
+
+        self._backward_times = [0] + [end-start for end,
+                                      start in zip(self._backward_times[1:], self._backward_times)]
+
+        return self._backward_times
+
+    def _indiv_layers(self):
+        def is_layer(module: nn.Module):
+            return len(module.children()) == 0
+
+        indiv_layers = filter(self.module.modules(), is_layer)
+        return dict(enumerate(indiv_layers))
 
     def _wrap_individual_layers(self, module: nn.Module, idx, layers_dict):
         for name, sub_module in module._modules.items():
+            # assume no cyclic routes in the network
+            # a module with no children is a layer
             if len(list(sub_module.children())) == 0:
                 module._modules[name] = Wrapper(sub_module, idx)
-                layers_dict[name] = module._modules[name]
+                layers_dict[idx] = module._modules[name]
                 idx += 1
             else:
                 idx, layers_dict = self._wrap_individual_layers(
