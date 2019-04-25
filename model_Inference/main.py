fixed backpropagation time estimation

model_Inference/main.py
@@ -1,31 +1,16 @@
-# %%
+
 import torch.nn as nn
 
 import torch
-import torchvision
 from network_profiler import NetProfiler
 
 from res_net_example import resnet20_cifar
 # hiddenlayer.build_graph(resnet20_cifar(), torch.zeros(1, 3, 32, 32))
 
 
-class complexNet(nn.Module):
-    def __init__(self):
-        super(complexNet, self).__init__()
-
-        self.sub1 = nn.Sequential(
-            nn.Sequential(nn.Linear(2, 10)),
-            nn.Linear(10, 2), nn.ReLU(), nn.Linear(2, 4), nn.Sequential(nn.Linear(4, 2)))
-
-        self.sub2 = nn.Linear(2, 1)
-
-    def forward(self, x):
-        return self.sub2(self.sub1(x))
-
-
 if __name__ == "__main__":
 
     base_model = resnet20_cifar()
-    profiler = NetProfiler(base_model, torch.randn(3, 3, 32, 32))
-    torch.cuda.synchronize()
-    print("done")
+    profiler = NetProfiler(base_model, torch.randn(3, 3, 32, 32), device="cpu")
+    for t in profiler._backward_times:
+        print(t)

model_Inference/network_profiler.py
@@ -4,15 +4,19 @@ import timeit
 
 
 class Wrapper(nn.Module):
-    def __init__(self, sub_module: nn.Module, idx):
+    def __init__(self, sub_module: nn.Module, idx, device):
         super(Wrapper, self).__init__()
         self.module = sub_module
+        self.device = device
         self.idx = idx
         self.forward_time = None
-        self.backward_time = None
+        self.backward_timestamp = None
 
-        def time_hook(__, _, ____):
-            self.backward_time = timeit.time.time()
+        def time_hook(_, __, ___):
+            # record when the layer finished calculating gradients
+            if self.device == "cuda":
+                torch.cuda.synchronize()
+            self.backward_timestamp = timeit.time.time()
 
         self.module.register_backward_hook(time_hook)
 
@@ -63,7 +67,9 @@ class NetProfiler(nn.Module):
         self._backward_times = None
         self._forward_times = None
 
+        # second profiling is the best one due to gpu warmup
         self.profile(sample_input)
+        # self.profile(sample_input)
 
     def forward(self, x):
         out = x.to(self.device)
@@ -80,11 +86,13 @@ class NetProfiler(nn.Module):
         return out
 
     def _gather_backward_times(self):
+        # time is calculated by subtracting successive timestamps
+        # for lack of a better option the last layer performs backpropagation in 0 time
         self._backward_times = [
-            layer.backward_time for layer in self.layers.values()]
+            layer.backward_timestamp for layer in self.layers.values()]
 
-        self._backward_times = [0] + [end-start for end,
-                                      start in zip(self._backward_times[1:], self._backward_times)]
+        self._backward_times = [
+            end-start for end, start in zip(self._backward_times, self._backward_times[1:])]+[0]
 
     def _indiv_layers(self):
         def is_layer(module: nn.Module):
@@ -98,7 +106,7 @@ class NetProfiler(nn.Module):
             # assume no cyclic routes in the network
             # a module with no children is a layer
             if len(list(sub_module.children())) == 0:
-                module._modules[name] = Wrapper(sub_module, idx)
+                module._modules[name] = Wrapper(sub_module, idx, self.device)
                 layers_dict[idx] = module._modules[name]
                 idx += 1
             else:
@@ -115,7 +123,11 @@ class NetProfiler(nn.Module):
         self._layer_sizes = [layer.size for layer in self.layers.values()]
 
         # perform symbolic forward run
+        if self.device == "cuda":
+            torch.cuda.synchronize()
+
         out = self(sample_input)
+
         if self.device == "cuda":
             torch.cuda.synchronize()
 
@@ -125,7 +137,11 @@ class NetProfiler(nn.Module):
 
         # perform symbolic backward run
         loss = out.norm()
+        if self.device == "cuda":
+            torch.cuda.synchronize()
+
         loss.backward()
+
         if self.device == "cuda":
             torch.cuda.synchronize()
 