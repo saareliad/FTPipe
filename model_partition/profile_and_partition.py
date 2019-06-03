80606c4557b0d74ea5615f355d7b452c87b63677
alondj <alondej@gmail.com>
2019-06-03 20:54:55 +0300

    distribute_model

--- model_partition/profile_and_partition.py
+++ model_partition/profile_and_partition.py
@@ -1,5 +1,16 @@
 from model_partition.network_profiler import profileNetwork
 from model_partition.graph import partition_model
+from model_partition.distribute_to_gpus import move_to_devices
+
+
+def distribute_model(model, device_list, *sample_batch, num_iter=4, max_depth=100, basic_blocks=None, trace_device="cuda"):
+    graph, _, _ = partition_network_using_profiler(model, num_gpus=len(device_list), *sample_batch, num_iter=num_iter,
+                                                   max_depth=max_depth, basic_blocks=basic_blocks, device=trace_device)
+
+    distributed_model = move_to_devices(
+        model, max_depth, basic_blocks, device_list, graph)
+
+    return distributed_model, graph
 
 
 def partition_network_using_profiler(model, num_gpus, *sample_batch, num_iter=4, max_depth=100, basic_blocks=None, device="cuda"):
