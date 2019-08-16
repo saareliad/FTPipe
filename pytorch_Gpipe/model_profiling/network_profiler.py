fixed profiling gradient creation

pytorch_Gpipe/model_profiling/network_profiler.py
@@ -79,7 +79,7 @@ def _perform_forward_backward_pass(net, *sample_batch: Tensors):
         torch.cuda.synchronize(device=device)
     else:
         out = net(*sample_batch)
-
+    net.zero_grad()
     return out
 
 

pytorch_Gpipe/utils.py
@@ -118,7 +118,7 @@ def find_output_shapes_of_scopes(model, scopes, *sample_batch: Tensors) -> Dict:
 
     with torch.no_grad():
         model(*sample_batch)
-
+        model.zero_grad()
     scope_to_shape = {}
     for layer, scope, parent in traverse_model(model, full=True):
         if isinstance(layer, ShapeWrapper):