d1ea9b9b138e5c90c9f7951f4a9ff8a715c29bec
benbanuz <benbanuz@users.noreply.github.com>
2019-08-25 20:06:29 +0300

    graph partition

--- tests/test_amoeba.py
+++ tests/test_amoeba.py
@@ -2,12 +2,15 @@ from sample_models.amoebaNet import AmoebaNet_D
 from experiments.graph_serialization import serialize_graph, deserialize_graph
 import torch
 from pytorch_Gpipe import distribute_using_profiler
+from pytorch_Gpipe.pipeline.pipeline_parallel import PipelineParallel
+from pytorch_Gpipe import distribute_by_time
 
 
-def check_shape(net):
+def check_shape():
     num_parts = [2, 4]
     num_runs = 10
 
+    net = AmoebaNet_D()
     model = net.to('cuda:0')
     x_tag = torch.randn((3, 3, 256, 256), device='cuda:0')
     y_tag = model(x_tag)
@@ -17,19 +20,23 @@ def check_shape(net):
 
         for _ in range(num_runs):
             x = torch.randn((3, 3, 256, 256), device=devices[0])
+            net = AmoebaNet_D()
 
-            model, _, _, graph = distribute_using_profiler(
-                net, torch.zeros(3, 3, 256, 256, device=devices[0]),
-                optimize_pipeline_wrappers=True, devices=devices, max_depth=100,
-                basic_blocks=None)
+            modified_model, wrappers, counter, _ = distribute_by_time(net, x,
+                                                                      devices=devices, depth=100,
+                                                                      optimize_pipeline_wrappers=True)
+            in_shape = x.shape[1:]
+            in_shape = tuple(in_shape)
 
-            y = model(x)
+            pipe = PipelineParallel(modified_model, 1, in_shape, wrappers, counter)
+
+            y = pipe(x)
 
             if y.shape != y_tag.shape:
                 print("different shape orig_shape:", y_tag.shape, " out_shape:", y.shape, " num_gpus = ", n_parts)
 
 
-def check_backward(net):
+def check_backward():
     num_parts = [2, 4]
     num_runs = 10
 
@@ -38,14 +45,17 @@ def check_backward(net):
             devices = [f'cuda:{i}' for i in range(n_parts)]
 
             for _ in range(num_runs):
-                x = torch.randn((3, 3, 256, 256))
+                x = torch.randn((3, 3, 256, 256), device=devices[0])
+                net = AmoebaNet_D()
 
-                model, _, _, graph = distribute_using_profiler(
-                    net, torch.zeros(3, 3, 256, 256, device=devices[0]),
-                    optimize_pipeline_wrappers=True, devices=devices, max_depth=100,
-                    basic_blocks=None)
+                modified_model, wrappers, counter, graph = distribute_by_time(net, x,
+                                                                              devices=devices, depth=100,
+                                                                              optimize_pipeline_wrappers=True)
+                in_shape = x.shape[1:]
+                in_shape = tuple(in_shape)
 
-                y = model(x)
+                pipe = PipelineParallel(modified_model, 1, in_shape, wrappers, counter)
+                y = pipe(x)
                 y.backward()
     except():
         serialize_graph(graph, "amoeba_net_backward.p")
@@ -53,5 +63,5 @@ def check_backward(net):
 
 if __name__ == '__main__':
     net = AmoebaNet_D()
-    check_shape(net)
-    check_backward(net)
+    check_shape()
+    check_backward()
