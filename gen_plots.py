c30c6e8ed1d55732624d7040571cd0aa42ff9289
Saar Eliad <saareliad@campus.technion.ac.il>
2019-12-31 12:00:14 +0000

    simpler plotting

--- gen_plots.py
+++ gen_plots.py
@@ -15,45 +15,6 @@ def add_plot(fn, legened, fig=None, plot_fn=plot.plot_fit):
     return fig, ax
 
 
-def current_status():
-    plot_fn = plot.plot_fit
-    fig = None
-
-    fig, ax = add_plot("results/stale_weights_ninja4.json",
-                       "ninja_stale_weights", fig=fig, plot_fn=plot_fn)
-    fig, ax = add_plot(
-        "results/ninja_msnag_clone_chunks_after_fix.json", "ninja_msnag", fig=fig, plot_fn=plot_fn)
-
-    # fig, ax = add_plot("results/stale_weights_rambo2_1.json", "rambo_stale_weights", fig=fig)
-    fig, ax = add_plot("results/stale_weights_rambo2.json",
-                       "rambo_stale_weights", fig=fig)  # Newer than above
-
-    fig, ax = add_plot(
-        "results/rambo2_msnag_clone_chunks_after_fix.json", "rambo_msnag", fig=fig, plot_fn=plot_fn)
-
-    # Memory efficient msnag
-    fig, ax = add_plot("results/ninja_msnag_calc.json",
-                       "ninja_msnag_calc", fig=fig)
-
-    # Gap aware:
-    fig, ax = add_plot("results/dummy_ga.json", "gap_aware",
-                       fig=fig, plot_fn=plot_fn)
-
-    gen_plot(out_dir='results', out_base_name='current_status.png')
-
-
-def gap_aware():
-    plot_fn = plot.plot_fit
-    fig = None
-    # Gap aware:
-    fig, ax = add_plot("results/dummy_ga.json", "gap_aware",
-                       fig=fig, plot_fn=plot_fn)
-    fig, ax = add_plot("results/dummy_ga_no_wd.json",
-                       "gap_aware_no_wd", fig=fig, plot_fn=plot_fn)
-
-    gen_plot(out_dir='results', out_base_name='gap_aware.png')
-
-
 def gen_plot(out_dir='results', out_base_name='current_status.png'):
     plt.plot()
     out_file_name = os.path.join(out_dir, out_base_name)
@@ -61,81 +22,42 @@ def gen_plot(out_dir='results', out_base_name='current_status.png'):
     print(f"-I- Generated: \"{out_file_name}\"")
 
 
-def four_partitions():
-    plot_fn = plot.plot_fit
-    fig = None
-    d = dict(fig=fig, plot_fn=plot_fn)
-    d['fig'], ax = add_plot("results/wrn_16x4_p4_stale_weights_1.json",
-                            "4p_stale_5_warmup", fig=fig, plot_fn=plot_fn)
-    d['fig'], ax = add_plot(
-        "results/wrn_16x4_p4_stale_weights_15_warmup.json", "4p_stale_15_warmup", **d)
-
-    d['fig'], ax = add_plot(
-        "results/wrn_16x4_p4_stale_weights_35_warmup.json", "4p_stale_35_warmup", **d)
-    d['fig'], ax = add_plot(
-        "results/wrn_16x4_p4_stale_weights_35_warmup_low_lr.json", "4p_stale_35_warmup_low_lr", **d)
-
-    d['fig'], ax = add_plot(
-        "results/wrn_16x4_p4_msnag_weights_35_warmup.json", "4p_msnag_35_warmup", **d)
-
-    d['fig'], ax = add_plot(
-        "results/wrn_16x4_p4_stale_weights_5_warmup_025_grad_norm.json", "4p_stale_5_warmup_025_clip", **d)
-    d['fig'], ax = add_plot(
-        "results/wrn_16x4_p4_msnag_weights_5_warmup_025_grad_norm.json", "4p_msnag_5_warmup_025_clip", **d)
-
-    # TODO: add after run done, change name to json json
-
-    gen_plot(out_dir='results', out_base_name='4p.png')
-
-
-def four_partitions_grad_norm():
-
-    plot_fn = plot.plot_grad_norm
-    fig = None
-    d = dict(fig=fig, plot_fn=plot_fn)
-    d['fig'], ax = add_plot(
-        "results/wrn_16x4_p4_stale_weights_5_warmup_025_grad_norm.json", "4p_stale_5_warmup_025_clip", **d)
-    d['fig'], ax = add_plot(
-        "results/wrn_16x4_p4_msnag_weights_5_warmup_025_grad_norm.json", "4p_msnag_5_warmup_025_clip", **d)
-    gen_plot(out_dir='results', out_base_name='4p_grad_norm.png')
-
-
-def debug_4p_with_grad_norm():
+def wrn_16x4_4p_with_grad_norm():
     d = dict(fig=None, plot_fn=plot.plot_grad_norm)
-    # 5 warm up  for all
-    d['fig'], ax = add_plot(
-        "results/stale.json", "4p_stale", **d)
-    d['fig'], ax = add_plot(
-        "results/stale_clip.json", "4p_stale_clip", **d)
-    d['fig'], ax = add_plot(
-        "results/stale_low_lr.json", "4p_low_lr", **d)
-    
-    gen_plot(out_dir='results', out_base_name='4p_debug_grad_norm.png')
+    # 5 warm up for all
+    fn_to_contour = {
+        "results/stale.json": "4p_stale",
+        "results/stale_clip.json": "4p_stale_clip",
+        "results/stale_low_lr.json": "4p_low_lr",
+        "results/msnag.json": "4p_msnag",
+        "results/msnag_ga.json": "4p_msnag+ga",
+    }
+
+    for n, c in fn_to_contour.items():
+        d['fig'], ax = add_plot(n, c, **d)
+
+    gen_plot(out_dir='results', out_base_name='4p_wrn16x4_grad_norm.png')
 
 
-def debug_4p_fit_plot():
+def wrn_16x4_4p_with_fit_plot():
 
     d = dict(fig=None, plot_fn=plot.plot_fit)
     # 5 warm up  for all
-    d['fig'], ax = add_plot(
-        "results/stale.json", "4p_stale", **d)
-    d['fig'], ax = add_plot(
-        "results/stale_clip.json", "4p_stale_clip", **d)
-    d['fig'], ax = add_plot(
-        "results/stale_low_lr.json", "4p_low_lr", **d)
-    
-    gen_plot(out_dir='results', out_base_name='debug_4p_fit_plot.png')
+    fn_to_contour = {
+        "results/stale.json": "4p_stale",
+        "results/stale_clip.json": "4p_stale_clip",
+        "results/stale_low_lr.json": "4p_low_lr",
+        "results/msnag.json": "4p_msnag",
+        "results/msnag_ga.json": "4p_msnag+ga",
+    }
+
+    for n, c in fn_to_contour.items():
+        d['fig'], ax = add_plot(n, c, **d)
+
+    gen_plot(out_dir='results', out_base_name='4p_wrn16x4_fit_plot.png')
 
 
 if __name__ == "__main__":
-    # current_status()
-    # gap_aware()
-    # four_partitions()
-    # four_partitions_grad_norm()
-
-
-    debug_4p_with_grad_norm()
-    debug_4p_fit_plot()
-
-    
+    wrn_16x4_4p_with_grad_norm()
+    wrn_16x4_4p_with_fit_plot()
     exit(0)
