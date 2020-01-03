373269e178162d93bc05302db3c2a92b7fa2f3a7
alondj <alondej@gmail.com>
2020-01-03 12:15:34 +0200

    amoebanet works

--- pytorch_Gpipe/model_partitioning/process_partition.py
+++ pytorch_Gpipe/model_partitioning/process_partition.py
@@ -26,6 +26,7 @@ def post_process_partition(graph: Graph, part: List[int]):
 
     graph_root_fix(graph)
     remove_backward_edges(graph)
+    do_not_send_lists(graph)
 
     # TODO ensure_dag makes problems
     # make_partitions_change_only_at_end_of_scope(graph)
@@ -37,6 +38,33 @@ def post_process_partition(graph: Graph, part: List[int]):
     # TODO we disabled this optimization
     # fix_arithmetic_inputs(graph)
     return graph
+
+
+def cannonize_partition_indices(graph: Graph):
+    num_parts = len({n.part for n in graph.nodes})
+    num_taken = 0
+    model_inputs = [node for node in graph.nodes if node.type == NodeTypes.IN]
+    open_nodes = deque(model_inputs)
+    closed = set()
+    cannonical_parts = dict()
+
+    while num_taken < num_parts:
+        node = open_nodes.popleft()
+        if node in closed or node in open_nodes:
+            continue
+        if node.part not in cannonical_parts:
+            cannonical_parts[node.part] = num_taken
+            num_taken += 1
+
+        closed.add(node)
+        edges = node.out_nodes.union(node.in_nodes)
+        nodes = edges.difference(closed, set(open_nodes))
+        open_nodes.extendleft(nodes)
+
+    for node in graph.nodes:
+        node.part = cannonical_parts[node.part]
+
+    graph.num_parts = len(cannonical_parts)
 
 
 def graph_root_fix(graph: Graph):
@@ -71,6 +99,24 @@ def remove_backward_edges(graph: Graph):
         if not backward_edges:
             break
     return fixed
+
+
+def do_not_send_lists(graph: Graph):
+    for node in graph.nodes:
+        if not ('ListConstruct' in node.scope):
+            continue
+
+        # nodes that have a list input such as torch.cat
+        # TODO revisit
+        for out in node.out_nodes:
+            if out.part != node.part:
+                # fix the output
+                out.part = node.part
+                # fix the output other inputs
+                for i in out.in_nodes:
+                    i.part = node.part
+
+# TODO the follwing functions are deprecated (residual from before code generation) might be repurposed later
 
 
 def fix_arithmetic_inputs(graph: Graph):
@@ -189,33 +235,6 @@ def strongly_connected_components_iterative(vertices: List[int], edges: Dict[int
                         del stack[index[v]:]
                         identified.update(scc)
                         yield scc
-
-
-def cannonize_partition_indices(graph: Graph):
-    num_parts = len({n.part for n in graph.nodes})
-    num_taken = 0
-    model_inputs = [node for node in graph.nodes if node.type == NodeTypes.IN]
-    open_nodes = deque(model_inputs)
-    closed = set()
-    cannonical_parts = dict()
-
-    while num_taken < num_parts:
-        node = open_nodes.popleft()
-        if node in closed or node in open_nodes:
-            continue
-        if node.part not in cannonical_parts:
-            cannonical_parts[node.part] = num_taken
-            num_taken += 1
-
-        closed.add(node)
-        edges = node.out_nodes.union(node.in_nodes)
-        nodes = edges.difference(closed, set(open_nodes))
-        open_nodes.extendleft(nodes)
-
-    for node in graph.nodes:
-        node.part = cannonical_parts[node.part]
-
-    graph.num_parts = len(cannonical_parts)
 
 
 def make_partitions_change_only_at_end_of_scope(graph: Graph):
