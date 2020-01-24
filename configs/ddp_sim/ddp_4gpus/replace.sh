53c277b7fc252a84a7183be03eac16cbc0c57c2b
Saar Eliad <saareliad@campus.technion.ac.il>
2020-01-24 23:28:04 +0000

    fix lr to x4 for 4GPU ddp

--- configs/ddp_sim/ddp_4gpus/replace.sh
+++ configs/ddp_sim/ddp_4gpus/replace.sh
@@ -11,17 +11,10 @@ function add {
 }
 
 
-
-
-# replace wrn_28x10_c100_dr03_p4 wrn_16x4_c100_dr03_p4 
-# replace results 'results\/4partitions\/wrn16x4_cifar100'
-# add  "\"logdir\": \"logs\/\"" "\"data_dir\": \"\/home_local\/saareliad\/data\","
-
 # add  "\"bs_train\": 128" "\"ddp_sim_num_gpus\": 4,"
-replace "\"out_filename\": \"seq\"" "\"out_filename\": \"ddp_sim\""
-replace "\"bs_train\": 128" "\"bs_train\": 512"
-
-# replace wrn_16x4_c100_dr03_p4 wrn_16x4_c100_p4
-
+# replace "\"out_filename\": \"seq\"" "\"out_filename\": \"ddp_sim\""
+# replace "\"bs_train\": 128" "\"bs_train\": 512"
+replace "\"lr\": 0.1" "\"lr\": 0.4"
 # replace "\"epochs\": 200" "\"epochs\": 220"
+
 
