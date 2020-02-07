9aab46fd4b2d5d2777f5956c2eadd98c8b316091
Saar Eliad <saareliad@campus.technion.ac.il>
2020-02-07 10:44:50 +0200

    script to print and understand whats going on  inside scheduler

--- pipeline/work_schedulers/schedulers.py
+++ pipeline/work_schedulers/schedulers.py
@@ -1,5 +1,5 @@
 import abc
-
+import pprint
 
 class WorkScheduler(abc.ABC):
     def __init__(self, step_every):
@@ -170,9 +170,9 @@ def expected_staleness(done_fwds, done_bwds, se):
     return sum([should_do_step(x, se) for x in range(done_bwds, done_fwds)])
 
 
-def my_version(batch_index, se):
+def my_version(done_bwds, se):
     """ steps so far """
-    return sum([should_do_step(i, se) for i in range(batch_index)])
+    return sum([should_do_step(i, se) for i in range(done_bwds)])
 
 
 def expected_version(done_fwds, done_bwds, se):
@@ -184,6 +184,33 @@ def backward_version(done_fwds, done_bwds, se):
     return my_version(done_bwds, se) + expected_staleness(done_fwds, done_bwds, se)
     # stage 0, len 4: [expected_staleness(i, max(i-3,0), 2) for i in range(10)]
 
+
+def get_staleness_for_stage(stage, scheduler, num_stages, num_batches, se):
+    s = print_for_stage(stage, scheduler, num_stages, num_batches)
+    d = {}
+    done_fwds = 0
+    done_bwds = 0
+    for c in s:
+        if c == 'F':
+            es = expected_staleness(done_fwds, done_bwds, se)
+            mv = my_version(done_bwds, se)
+            ev = expected_version(done_fwds, done_bwds, se)
+            bv = mv + es
+            mys = s[:done_fwds+done_bwds+1]
+            d[done_fwds]= dict(es=es, mv=mv, ev=ev, bv=bv, mys=mys)
+            done_fwds +=1
+        if c == 'B':
+            steps_so_far = done_bwds // se
+            if not (steps_so_far == d[done_bwds]['bv']):
+                # pprint.pprint(d)
+                raise AssertionError(f"Stage:{stage}, batch:{done_bwds}, steps_so_far:{steps_so_far}, but predicted: {d[done_bwds]['bv']}.\n \
+                    Extra:\n {pprint.pformat(d)},\n {s[:done_fwds+done_bwds+1]}")
+             
+            done_bwds+=1
+
+    return d
+
+
 # print([expected_version(i, max(i-3,0), 2) for i in range(15)]); print(list(range(15))); print([expected_staleness(i, max(i-3,0), 2) for i in range(15)]);
 
 
@@ -192,9 +219,12 @@ if __name__ == "__main__":
     EXTRA = 5
     # stage = 0  # Should test the edge case.
     num_batches = num_stages*2 + 1 + EXTRA
-    step_every = 2
-
+    step_every = 1
     sched_name = "1F1B"
+
+
+    d = dict(sched_name=sched_name, step_every=step_every, num_stages=num_stages)
+    print(f"-I- got args: {pprint.pformat(d)}")
     AVAILABLE_WORK_SCHEDULERS = {"1F1B": FBScheduler,
                                  "SEQ": SeqScheduler,
                                  "GPIPE": GpipeScheduler,
@@ -227,3 +257,9 @@ if __name__ == "__main__":
     print(stage_fwds_between_first_step)
     print()
     print("Problematic stages:", stage_fwds_problematic)
+
+
+    for stage in range(num_stages):
+        print(f"Stage {stage}")
+        d = get_staleness_for_stage(stage, scheduler, num_stages, num_batches, step_every)
+        pprint.pprint(d)
