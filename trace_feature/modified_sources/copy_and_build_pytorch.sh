594638f5bddf8206eeaccabac094f74adaed68d5
Saar Eliad <saareliad@campus.technion.ac.il>
2020-02-18 15:25:07 +0200

    Resnets and cosmetics. working towards comparing partitioning

--- trace_feature/modified_sources/copy_and_build_pytorch.sh
+++ trace_feature/modified_sources/copy_and_build_pytorch.sh
@@ -1,7 +1,12 @@
 
-PYTORCH_LOCATION=/home/alonde-jager/gpipe-research/pytorch
-# git clone https://github.com/pytorch/pytorch.git $PYTORCH_LOCATION
-# cd $PYTORCH_LOCATION; git checkout v1.4.0 ; cd -
+# PYTORCH_LOCATION=/home/alonde-jager/gpipe-research/pytorch
+PYTORCH_LOCATION=/home_local/saareliad/pytorch
+
+git clone https://github.com/pytorch/pytorch.git $PYTORCH_LOCATION
+cd $PYTORCH_LOCATION; git checkout v1.4.0 ; cd -
+
 rsync pytorch/ ${PYTORCH_LOCATION}/ -ravh
 cd $PYTORCH_LOCATION
 docker build -t pytorch -f docker/pytorch/Dockerfile .
+cd -
+
