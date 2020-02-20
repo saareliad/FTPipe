Many important changes: supporting save_memory_feature. Towards fixing some problems in timing/profiling for recomputation. Save memory by zeroing gradients after each layer. Fix hueristics function (previously, all times were close to 1, now multiplied by 100 for the hueristic) Supporting METIS opts (practically) with some interesting features.
Note that the "compress" option which was default to True by nxmetis changed to False

pytorch_Gpipe/__init__.py
@@ -27,7 +27,9 @@ def pipe_model(model: nn.Module,
                use_layers_only_graph: bool = False,
                output_file: str = None,
                DEBUG=False,
-               **METIS_opt) -> Graph:
+               recomputation=False,
+               save_memory_mode=False,
+               METIS_opt=dict()) -> Graph:
     '''attemps to partition a model to given number of parts using our profiler
        this will produce a python file with the partition config
 
@@ -63,7 +65,7 @@ def pipe_model(model: nn.Module,
     DEBUG:
         whether to generate the debug version of the partition more comments and assertions in the generated file
     METIS_opt:
-        additional kwargs to pass to the METIS partitioning algorithm
+        dict of additional kwargs to pass to the METIS partitioning algorithm
     '''
 
     graph = partition_model(model,
@@ -76,6 +78,8 @@ def pipe_model(model: nn.Module,
                             node_weight_function=node_weight_function,
                             edge_weight_function=edge_weight_function,
                             use_layers_only_graph=use_layers_only_graph,
+                            recomputation=recomputation,
+                            save_memory_mode=save_memory_mode,
                             METIS_opt=METIS_opt)
 
     compile_partitoned_model(graph,
@@ -96,7 +100,9 @@ def partition_model(model: nn.Module,
                     node_weight_function: Optional[NodeWeightFunction] = None,
                     edge_weight_function: Optional[EdgeWeightFunction] = None,
                     use_layers_only_graph: bool = False,
-                    **METIS_opt) -> Graph:
+                    recomputation: bool = False,
+                    save_memory_mode: bool = False,
+                    METIS_opt=dict()) -> Graph:
     '''
     profiles the network and return a graph representing the partition
 
@@ -124,14 +130,16 @@ def partition_model(model: nn.Module,
     use_layers_only_graph:
         whether to partition a smaller version of the graph containing only the layers (usefull fo big models with lots of unprofiled ops)
     METIS_opt:
-        additional kwargs to pass to the METIS partitioning algorithm
+        dict of additional kwargs to pass to the METIS partitioning algorithm
     '''
     graph = build_graph(model,
                         sample_batch,
                         kwargs=kwargs,
                         max_depth=max_depth,
                         basic_blocks=basic_blocks,
-                        n_iter=n_iter)
+                        n_iter=n_iter,
+                        recomputation=recomputation,
+                        save_memory_mode=save_memory_mode)
 
     graph = METIS_partition(graph,
                             nparts,

pytorch_Gpipe/model_profiling/graph_builder.py
@@ -60,7 +60,9 @@ def build_graph(model: torch.nn.Module,
                 kwargs: Optional[Dict] = None,
                 max_depth: int = 1000,
                 basic_blocks: Optional[Tuple[torch.nn.Module, ...]] = None,
-                n_iter: int = 10) -> Graph:
+                n_iter: int = 10,
+                recomputation=False,
+                save_memory_mode=False) -> Graph:
     if kwargs is None:
         kwargs = dict()
         # TODO tracing not tested with kwargs
@@ -79,7 +81,9 @@ def build_graph(model: torch.nn.Module,
                                      kwargs=kwargs,
                                      n_iter=n_iter,
                                      max_depth=max_depth,
-                                     basic_blocks=basic_blocks)
+                                     basic_blocks=basic_blocks,
+                                     recomputation=recomputation,
+                                     save_memory_mode=save_memory_mode)
 
     tensors = tensorDict(model)
     profiled_layers = layerDict(model,
@@ -109,8 +113,9 @@ def build_graph(model: torch.nn.Module,
         # if extension not built fall back to a default solution
         # TODO remove it at some point as it's not a best practice
         warnings.warn(
-            "trace_feature not found. falling back to regular trace which is less accurate and might not work\n please build it"
-        )
+            "Trace_feature not found."
+            "Falling back to regular trace which is less accurate and might not work\n."
+            "Please build it.")
         torch._C._jit_pass_inline(trace_graph)
 
     global DEBUG_MODEL_NAME
@@ -539,7 +544,8 @@ def _remove_nodes_that_go_nowhere(nodes: GraphNodes,
             return False
 
         # if we have for example 2 unpacking and only the second is used then
-        # because we decide the unpacking index by position we cant remove the unused first unpacking as it will lead to the wrong index being used
+        # because we decide the unpacking index by position we cant remove the unused first unpacking
+        # as it will lead to the wrong index being used
         if "prim::TupleUnpack" in node.scope or "prim::ListUnpack" in node.scope:
             assert node.type is NodeTypes.PYTHON_PRIMITIVE
             return False
@@ -575,7 +581,7 @@ def _remove_nodes(nodes: GraphNodes, condition: Callable[[Node],
 
 
 def opMatch(scope: str, op_name: str) -> bool:
-    return re.search(f"{op_name}[{string.digits}]", scope) != None
+    return re.search(f"{op_name}[{string.digits}]", scope) is not None
 
 
 ###########################################
@@ -584,7 +590,8 @@ def opMatch(scope: str, op_name: str) -> bool:
 @DEBUG_DUMP_GRAPH
 def add_unpack_nodes(nodes: GraphNodes, to_fix: Dict[int, int]) -> GraphNodes:
     '''
-    when not all of a tuple elements are used it is possible that not all of the unpack nodes will be emitted by the trace
+    when not all of a tuple elements are used it is possible that
+    not all of the unpack nodes will be emitted by the trace,
     so we add them here if necessary
     '''
     new_graph = OrderedDict()
@@ -802,8 +809,11 @@ def _pack_inputs(node: Node, inputs, offset):
 
 
 def accessor_paths(outputs, path=()):
-    '''given a tuple yields the indices to access each element and if the nested element if terminal or iterable
-       for example l=(1,(6,(7,8))) will result in (0,) True , (1,) False, (1,0) True (1,1) False, (1,1,0) True, (1,1,1) True
+    '''
+    Given a tuple yields the indices to access each element and if the nested element if terminal or iterable
+    Example:
+        l=(1,(6,(7,8))) 
+        will result in (0,) True , (1,) False, (1,0) True (1,1) False, (1,1,0) True, (1,1,1) True
     '''
     for idx, val in enumerate(outputs):
         accessor = path + (idx, )