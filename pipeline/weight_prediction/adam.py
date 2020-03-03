6eb90a559b038893755a6d80f4276a208b84c092
saareliad <saareliad@campus.technion.ac.il>
2020-03-03 23:42:47 +0200

    Fix geometric sum, moved eps

--- pipeline/weight_prediction/adam.py
+++ pipeline/weight_prediction/adam.py
@@ -42,7 +42,7 @@ class AdamClonedWeightPrediction(WeightPredictor):
 
                     # Compute coefficient as sum of predictions.
                     # (1): compute the part related to step size, as it is too annoying to derive manually.
-                    # (2): + Geometric series sum of beta1.
+                    # (2): * Geometric series sum of beta1.
 
                     # (1)
                     momentum_coeff = 0
@@ -55,19 +55,13 @@ class AdamClonedWeightPrediction(WeightPredictor):
                         # NOTE: moved lr outside
                         # step_size = 1 / bias_correction1 -> included
                         # NOTE: move exp_avg outside
-                        momentum_coeff += (((math.sqrt(bias_correction2)) + eps) / bias_correction1) 
+                        # (2): (beta1 ** staleness)
+                        momentum_coeff += (math.sqrt(bias_correction2) / bias_correction1) * (beta1 ** staleness)
 
-                    # (2)
-                    # This line models the geometric series sum, which is created by
-                    # exp_avg.mul_(beta1).add_(1 - beta1, grad)
-                    momentum_coeff += (beta1 - math.pow(beta1, self.n_steps + 1)) / (1 - beta1)
-                    
-                    # Handle the case of division by zero.
-                    a = exp_avg / exp_avg_sq.sqrt()
-                    a[a != a] = 0
-
+                    denom = exp_avg_sq.sqrt().add_(eps)
+                    # NOTE: the eps is not used the exact same as normal adam. This should be negligible.
                     # Finally, calculate theta hat.
-                    p.add_(-lr * momentum_coeff, a)
+                    p.data.addcdiv_(-lr * momentum_coeff, exp_avg, denom)
 
     def revert(self):
         if not self.n_steps:
