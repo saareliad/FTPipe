066dcc24b28a53e21d66f771c8d753877424efe2
Saar Eliad <saareliad@campus.technion.ac.il>
2020-03-04 12:22:15 +0200

    Supporting LM trainer, rename cvtrainer to cv_trainer.

--- pipeline/training/__init__.py
+++ pipeline/training/__init__.py
@@ -1,5 +1,9 @@
-
 from .interface import AnyTrainer
-from .cvtrainer import CVTrainer, GapAwareCVTrainer
-AVAILABLE_TRAINERS = {'cv': CVTrainer, 'cv_gap_aware': GapAwareCVTrainer}
-# TODO: LM.
+from .cv_trainer import CVTrainer, GapAwareCVTrainer
+from .lm_trainer import LMTrainer, GapAwareLMTrainer
+AVAILABLE_TRAINERS = {
+    'cv': CVTrainer,
+    'cv_gap_aware': GapAwareCVTrainer,
+    'lm': LMTrainer,
+    'lm_gap_aware': GapAwareLMTrainer
+}
