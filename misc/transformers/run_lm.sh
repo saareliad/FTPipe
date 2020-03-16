e6513a62e0492e1e1edddf4eb5b52c748a31c399
Saar Eliad <saareliad@campus.technion.ac.il>
2020-03-16 20:10:21 +0200

    files used for ddp gpt2 results

---
+++ misc/transformers/run_lm.sh
@@ -0,0 +1,21 @@
+export TRAIN_FILE=/home_local/saareliad/data/wikitext-2-raw/wiki.train.raw
+export TEST_FILE=/home_local/saareliad/data/wikitext-2-raw/wiki.test.raw
+
+#592 
+#python run_language_modeling.py \
+python -m torch.distributed.launch --nproc_per_node 4 run_language_modeling.py \
+    --output_dir=output \
+    --model_type=gpt2 \
+    --model_name_or_path=gpt2 \
+    --do_train \
+    --train_data_file=$TRAIN_FILE \
+    --do_eval \
+    --num_train_epochs 3 \
+    --per_gpu_train_batch_size 1 \
+    --per_gpu_eval_batch_size 4 \
+    --gradient_accumulation_steps 4 \
+    --eval_data_file=$TEST_FILE \
+    --warmup_steps 0 \
+    --logging_step 592 \
+    --save_steps 0 \
+    --evaluate_during_training
