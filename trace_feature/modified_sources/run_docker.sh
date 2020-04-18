a4e8efc42efcfd285fa8291a496b52034040ff86
Saar Eliad <saareliad@campus.technion.ac.il>
2020-04-18 14:28:21 +0300

    run docker, open port for debug and Z).

--- trace_feature/modified_sources/run_docker.sh
+++ trace_feature/modified_sources/run_docker.sh
@@ -1,4 +1,5 @@
 # Run from main repo:
 # may change image name.
 # TODO: share cache for models/datasets
-docker run -it --ipc=host --rm -v $(pwd):/workspace partitioning/live
+# docker run -it --ipc=host --rm -v $(pwd):/workspace partitioning/live
+docker run -it --ipc=host --rm -p 12345:12345 -v $(pwd):/workspace:Z partitioning/live
