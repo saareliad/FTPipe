5fef4b1878cb311d275dcd3742bb931f8efb9ad1
Saar Eliad <saareliad@campus.technion.ac.il>
2020-04-18 19:02:28 +0300

    nicer exps

--- run_exp_multi_gpu.py
+++ run_exp_multi_gpu.py
@@ -18,20 +18,19 @@ def main():
                                   gpus_per_config=4)
 
 
-def main2():
+def gpt2_tied():
     COMMAND = "mpirun -np 5 python main.py"
     cfgs_dir = "configs/lm/wt2/gpt2/tied_weights/"
-    all_algs = ["stale"]
+    all_algs = ["stale", "seq", "ws", "msnag", "ws_msnag_ga", "ws_msnag_ga_jfl", "ws_msnag"]
 
     param_grid = {
         'config': [f"{cfgs_dir}{cfg}.json" for cfg in all_algs],
-        'seed': [20202020, 77777777, 314159, 1322019]
+        'seed': [42, 20202020, 77777777, 314159, 1322019]
     }
     run_grid_on_multi_gpu_per_run(COMMAND,
                                   param_grid,
                                   gpu_list=list(range(8)),
                                   gpus_per_config=4)
-
 
 def gpt2xl_untied():
     COMMAND = "mpirun -np 8 python main.py"
@@ -40,7 +39,6 @@ def gpt2xl_untied():
     # all_algs = ["stale", "msnag", "ws", "ws_msnag_ga", "ws_msnag_ga_jfl", "ws_msnag"]
     param_grid = {
         'config': [f"{cfgs_dir}{cfg}.json" for cfg in all_algs],
-        # 'seed': [42, 20202020, 77777777, 314159, 1322019]
         'seed': [42, 20202020, 77777777, 314159, 1322019]
     }
     run_grid_on_multi_gpu_per_run(COMMAND,
@@ -49,5 +47,6 @@ def gpt2xl_untied():
                                   gpus_per_config=8)
 
 if __name__ == "__main__":
-    gpt2xl_untied()
+    # gpt2xl_untied()
+    gpt2_tied()
     # main2()
