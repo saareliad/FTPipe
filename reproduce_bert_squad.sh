72814d72f99a119abbf879ed8cef388d101ed66c
Alon De-Jager <alonde-jager@rishon.csf.technion.ac.il>
2020-04-20 21:47:09 +0300

    added bert_large_uncased_4p

--- reproduce_bert_squad.sh
+++ reproduce_bert_squad.sh
@@ -1,5 +1,3 @@
 bash make_squad.sh
 export SQUAD_DIR=squad1
-python partition_squad_models.py --n_partitions 4 --partitioning_batch_size 8 --bwd_to_fwd_ratio 5 --n_iter 50 --auto_file_name --partition_layer_graph  --model_type bert   --model_name_or_path bert-base-uncased  --do_lower_case   --train_file $SQUAD_DIR/train-v1.1.json   --predict_file $SQUAD_DIR/dev-v1.1.json    --max_seq_length 384   --doc_stride 128 
-
-# FIXME: Fails.
+python partition_squad_models.py --n_partitions 4 --partitioning_batch_size 16 --bwd_to_fwd_ratio 5 --n_iter 50 --auto_file_name --model_type bert --model_name_or_path bert-large-uncased --do_lower_case --train_file $SQUAD_DIR/train-v1.1.json --predict_file $SQUAD_DIR/dev-v1.1.json --max_seq_length 384 --doc_stride 128
