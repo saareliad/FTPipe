a0e878f7e356da558861847a5bc196a829a642d5
Saar Eliad <saareliad@campus.technion.ac.il>
2020-04-21 13:20:31 +0300

    format

--- pipeline/weight_prediction/sgd.py
+++ pipeline/weight_prediction/sgd.py
@@ -15,31 +15,37 @@ class SGDRevertableLinearWeightPrediction(WeightPredictor):
     def __init__(self, *args, **kw):
         # TODO: work in interaction with "true_weights_storage" there is some hack to be done...
         raise NotImplementedError(
-            "SGDRevertableLinearWeightPrediction not yet supported for pipeline")
+            "SGDRevertableLinearWeightPrediction not yet supported for pipeline"
+        )
         super().__init__(*args, **kw)
 
     def forward(self):
         if not self.n_steps:
             return
         with torch.no_grad():
-            self.buffered_fixes = [self.fix_fn(
-                self, pg) for pg in self.optimizer.param_groups]
+            self.buffered_fixes = [
+                self.fix_fn(self, pg) for pg in self.optimizer.param_groups
+            ]
 
-            for pg, fix_fn_item in zip(self.optimizer.param_groups, self.buffered_fixes):
+            for pg, fix_fn_item in zip(self.optimizer.param_groups,
+                                       self.buffered_fixes):
                 if fix_fn_item:
                     for p in pg['params']:
-                        p.data.add_(-fix_fn_item,
-                                    self.optimizer.state[p]["momentum_buffer"].data)
+                        p.data.add_(
+                            -fix_fn_item,
+                            self.optimizer.state[p]["momentum_buffer"].data)
 
     def revert(self):
         if not self.n_steps:
             return
         with torch.no_grad():
-            for pg, fix_fn_item in zip(self.optimizer.param_groups, self.buffered_fixes):
+            for pg, fix_fn_item in zip(self.optimizer.param_groups,
+                                       self.buffered_fixes):
                 if fix_fn_item:
                     for p in pg['params']:
-                        p.data.add_(fix_fn_item,
-                                    self.optimizer.state[p]["momentum_buffer"].data)
+                        p.data.add_(
+                            fix_fn_item,
+                            self.optimizer.state[p]["momentum_buffer"].data)
 
 
 class SGDClonedWeightPrediction(WeightPredictor):
@@ -82,7 +88,6 @@ class SGD2MSNAG(FixFunction):
     Mentioned as eq 10 Goyal et al.
     Fixed with MSNAG
      """
-
     def __call__(self, p: WeightPredictor, pg):
         gamma = pg['momentum']
         if p.n_steps == 1:
@@ -93,7 +98,6 @@ class SGD2MSNAG(FixFunction):
 
 class SGD1MSNAG(SGD2MSNAG):
     """ Pytorch SGD. Mentioned as eq 9 Goyal et al. """
-
     def __call__(self, p: WeightPredictor, pg):
         return pg['lr'] * super().__call__(p, pg)
 
@@ -104,14 +108,13 @@ PRED_MEM_TO_CLASS = {
 }
 
 # FIXME: keys are hardcoded from optimizers...
-SGD_TYPE_TO_MSNAG_CLASS = {
-    'sgd1': SGD1MSNAG,
-    'sgd2': SGD2MSNAG
-}
+SGD_TYPE_TO_MSNAG_CLASS = {'sgd1': SGD1MSNAG, 'sgd2': SGD2MSNAG}
 
 
-def get_sgd_weight_predictor(sgd_type: str, pred_mem: str,
-                             optimizer, scheduler=None,
+def get_sgd_weight_predictor(sgd_type: str,
+                             pred_mem: str,
+                             optimizer,
+                             scheduler=None,
                              nag_with_predictor=False,
                              true_weights_storage=None) -> WeightPredictor:
     has_weight_decay = any(
@@ -119,9 +122,12 @@ def get_sgd_weight_predictor(sgd_type: str, pred_mem: str,
     if has_weight_decay:
         if sgd_type == 'sgd1':
             if pred_mem == 'clone':
-                return SGDWDClonedWeightPrediction(optimizer, fix_fn=None, scheduler=scheduler,
-                                                   nag_with_predictor=nag_with_predictor,
-                                                   true_weights_storage=true_weights_storage)
+                return SGDWDClonedWeightPrediction(
+                    optimizer,
+                    fix_fn=None,
+                    scheduler=scheduler,
+                    nag_with_predictor=nag_with_predictor,
+                    true_weights_storage=true_weights_storage)
             else:
                 raise NotImplementedError()
         else:
@@ -132,6 +138,8 @@ def get_sgd_weight_predictor(sgd_type: str, pred_mem: str,
         pred_cls = PRED_MEM_TO_CLASS.get(pred_mem, None)
         # pred_cls: WeightPredictor
         # fix_fn: FixFunction
-        return pred_cls(optimizer, fix_fn=fix_fn, scheduler=scheduler,
+        return pred_cls(optimizer,
+                        fix_fn=fix_fn,
+                        scheduler=scheduler,
                         nag_with_predictor=nag_with_predictor,
                         true_weights_storage=true_weights_storage)
