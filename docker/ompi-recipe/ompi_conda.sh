3cff5f4a343804c12f74fb17f7dc40a1cbc5cc75
Saar Eliad <saareliad@campus.technion.ac.il>
2020-04-22 22:46:09 +0300

    added upload line

--- docker/ompi-recipe/ompi_conda.sh
+++ docker/ompi-recipe/ompi_conda.sh
@@ -1,3 +1,4 @@
+# without docker:
 # wget https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-4.0.3.tar.gz
 # tar -xvzf openmpi-4.0.3.tar.gz ompi
 
@@ -5,6 +6,9 @@ docker build -t conda:ompi-dev . && docker run --rm -it --runtime nvidia conda:o
 # inside-container# anaconda login
 # inside-container# conda build ompi-cuda
 # inside-container# anaconda upload /path/to/ompi-cuda/package.tar.gz
+# anaconda upload /opt/conda/conda-bld/noarch/se-msnag2-4.0.3-cuda10.2.tar.bz2
+
+
 
 # # The path is determind by prefix env var (?)
 # # export PREFIX=''
