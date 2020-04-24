905e4633918a88b2009d9c6a70c1eac9a46f4730
alondj <alondej@gmail.com>
2020-04-24 16:17:47 +0300

    trace arithmetic operations currently disallow logical operators

--- tracer.py
+++ tracer.py
@@ -19,13 +19,13 @@ from collections import deque
 # record all tensor creation ops
 # write a model tracer which records current context
 # record basic operations on wrapped values
+# record layers
 
 
 # TODO record kwargs
 # TODO record constants
 # TODO record lists and tuples
 # TODO record which function was called
-# TODO record layers
 
 
 # NOTE this should be set after we modify tensor creation functions like torch.cat
@@ -38,7 +38,7 @@ IN_EGDES = defaultdict(list)
 OUT_EDGES = defaultdict(list)
 
 
-DEBUG_CREATED_SCOPES = deque()
+DEBUG_CREATED_SCOPES = dict()
 
 
 def record_edge(src, dest):
@@ -52,16 +52,20 @@ def record_edge(src, dest):
 
 
 def record_edges(args, kwargs, out):
+    """connect all edges between traced args and kwargs to the output
+    """
     for a in chain(flatten(args), flatten(kwargs)):
-        assert isinstance(a, (TracedValue, TracedFunction))
+        assert isinstance(a, TracedValue)
         record_edge(a.id, out.id)
 
 
 def connect_inputs_to_output(connect_self=True):
+    """decorator to auto add edges between traced input and kwargs of function to it's output
+       if connect_self is True will also record the connection between the self of the caller to the output
+    """
     def connect(func):
         @wraps(func)
         def wrapper(self_arg, *args, **kwargs):
-            args, kwargs = wrap_args_and_kwargs(*args, **kwargs)
             out = func(self_arg, *args, **kwargs)
 
             if connect_self:
@@ -74,6 +78,8 @@ def connect_inputs_to_output(connect_self=True):
 
 
 def ensure_traced_args_and_kwargs(func):
+    """a decorator that ensures that all inputs of a function are traced
+    """
     @wraps(func)
     def wrapper(self_arg, *args, **kwargs):
         args, kwargs = wrap_args_and_kwargs(*args, **kwargs)
@@ -146,7 +152,10 @@ class TracedTensorProducingFunction():
 
 
 @contextmanager
-def patch_tensor_functions():
+def patch_tensor_creating_functions():
+    """context manager that ensures that tensors which are created using torch functions like torch.zeros
+    are traced
+    """
     # Before yield as the enter method
     patched_functions = [TracedTensorProducingFunction(torch, f)
                          for f in TENSOR_PRODUCING_FUNCTIONS]
@@ -165,18 +174,27 @@ def patch_tensor_functions():
 
 
 def isTracedValue(data):
+    """
+    predicate to check if a value can be traced
+    """
     return isinstance(data, (list, tuple, int, bool, str, float,
                              torch.device, torch.Size, torch.Tensor,
                              torch.dtype, torch.memory_format))
 
 
 def unwrap_args_and_kwargs(*args, **kwargs):
+    """
+    retrives the traced data from args and kwargs untraced data is returned as is 
+    """
     args = unwrap_args(args)
     kwargs = unwrap_kwargs(kwargs)
     return args, kwargs
 
 
 def wrap_args_and_kwargs(*args, **kwargs):
+    """recursively ensure that all arguments passed in args and kwargs are traced
+       if a value was not traced it will be recorded as a constant
+    """
     args = wrap_args(args)
     kwargs = wrap_kwargs(kwargs)
     return args, kwargs
@@ -219,6 +237,8 @@ def unwrap_kwargs(kwargs):
 
 
 def flatten(args):
+    """return all nested data and tracedValues from a nested iterable
+    """
     if isinstance(args, TracedValue):
         yield args
     elif isinstance(args, (list, set, tuple)):
@@ -229,6 +249,71 @@ def flatten(args):
             yield from flatten(a)
     else:
         yield args
+
+
+def reduce_inputs(func):
+    """
+    decorator which ensures that func is called with unwrapped inputs
+    """
+    @wraps(func)
+    def wrapper(*wrapped_args, **wraped_kwargs):
+        args, kwargs = unwrap_args_and_kwargs(*wrapped_args, **wraped_kwargs)
+        return func(*args, **kwargs)
+    return wrapper
+
+
+def call_traced_fn(func):
+    """a decorator which delegates function call to the wrapped value
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        func_name = func.__name__
+        actual_func = getattr(type(args[0]), func_name)
+        out = actual_func(*args, **kwargs)
+        return out
+    return wrapper
+
+
+def wrap_output(func):
+    """a decorator that auto wraps the output of func in a TracedValue
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        namespace = type(args[0]).__name__
+        op = func.__name__
+        out = func(*args, **kwargs)
+        return TracedValue(out, f"{namespace}::{op}")
+    return wrapper
+
+
+def wrap_magic(func):
+    """
+    a decorator that auto records magic methods
+
+    @wrap_magic
+    def __add__(self,other):
+        pass
+
+    will ensure to record the add operator as called with the traced value
+    """
+    return ensure_traced_args_and_kwargs(
+        connect_inputs_to_output(connect_self=True)(
+            reduce_inputs(
+                wrap_output(
+                    call_traced_fn(func)
+                ))))
+
+
+def tracing_not_supported(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        namespace = type(args[0]).__name__
+        op = func.__name__
+
+        msg = f"tracing {namespace}::{op} is currently not supported"
+        raise NotImplementedError(msg)
+
+    return reduce_inputs(wrapper)
 
 
 class TracedValue(object):
@@ -248,9 +333,13 @@ class TracedValue(object):
         self.namespace = f"{type(self._data).__name__}"
         self.id = TracedValue.ID
         TracedValue.ID += 1
-        self.scope = SCOPE + f"/{creating_op}"
 
-        DEBUG_CREATED_SCOPES.append(self.scope)
+        if creating_op:
+            self.scope = SCOPE + f"/{creating_op}"
+        else:
+            self.scope = SCOPE
+
+        DEBUG_CREATED_SCOPES[self.id] = self.scope
 
     def __repr__(self):
         return f"Node ID:{self.id}\nScope:{self.scope}\nvalue: {self._data}\n"
@@ -284,52 +373,136 @@ class TracedValue(object):
 
         return TracedFunction(self.id, self.namespace, out)
 
-    # TODO auto patch __magic__ methods
+    ##############################
+    # Magic Method delegation
+    # this is either very clever or a huge abuse of decorators
+    ##############################
 
-    @ensure_traced_args_and_kwargs
-    @connect_inputs_to_output(connect_self=True)
+    @wrap_magic
     def __getitem__(self, idx):
-        out = self._data[idx._data]
-        out = TracedValue(
-            out, creating_op=f"{self.namespace}::unpack[{idx._data}]")
-        return out
+        pass
+
     ##############################
     # Arithmetic operations
     ##############################
-    @ensure_traced_args_and_kwargs
-    @connect_inputs_to_output(connect_self=True)
+    @wrap_magic
     def __add__(self, other):
-        out = TracedValue(data=self._data + other._data,
-                          creating_op=f"{self.namespace}::add")
-        return out
+        pass
 
-    @ensure_traced_args_and_kwargs
-    @connect_inputs_to_output(connect_self=True)
+    @wrap_magic
     def __radd__(self, other):
-        return self + other
+        pass
 
-    @ensure_traced_args_and_kwargs
-    @connect_inputs_to_output(connect_self=True)
-    def __mul__(self, other):
-        out = TracedValue(data=self._data * other._data,
-                          creating_op=f"{self.namespace}::mul")
-        return out
-
-    @ensure_traced_args_and_kwargs
-    @connect_inputs_to_output(connect_self=True)
-    def __rmul__(self, other):
-        return self * other
-
-    @ensure_traced_args_and_kwargs
-    @connect_inputs_to_output(connect_self=True)
+    @wrap_magic
     def __iadd__(self, other):
-        self._data += other._data
+        pass
 
-        # we create a new wrapper that shares the sampe data as the original
-        # this is done so we can record the connection between the inputs
-        # and the output (we cannot add a self edge)
-        out = TracedValue(self._data, creating_op=f"{self.namespace}::iadd")
-        return out
+    @wrap_magic
+    def __sub__(self, other):
+        pass
+
+    @wrap_magic
+    def __rsub__(self, other):
+        pass
+
+    @wrap_magic
+    def __isub__(self, other):
+        pass
+
+    @wrap_magic
+    def __mul__(self, other):
+        pass
+
+    @wrap_magic
+    def __rmul__(self, other):
+        pass
+
+    @wrap_magic
+    def __imul__(self, other):
+        pass
+
+    @wrap_magic
+    def __div__(self, other):
+        pass
+
+    @wrap_magic
+    def __rdiv__(self, other):
+        pass
+
+    @wrap_magic
+    def __idiv__(self, other):
+        pass
+
+    @wrap_magic
+    def __mod__(self, other):
+        pass
+
+    @wrap_magic
+    def __matmul__(self, other):
+        pass
+
+    @wrap_magic
+    def __pow__(self, other):
+        pass
+
+    @wrap_magic
+    def __truediv__(self, other):
+        pass
+
+    @wrap_magic
+    def __floordiv__(self, other):
+        pass
+
+    @wrap_magic
+    def __rfloordiv__(self, other):
+        pass
+
+    @wrap_magic
+    def __rshift__(self, other):
+        pass
+
+    @wrap_magic
+    def __lshift__(self, other):
+        pass
+
+    ##############################
+    # Logical operations
+    ##############################
+    @tracing_not_supported
+    def __and__(self, other):
+        return self.__and__(other)
+
+    @tracing_not_supported
+    def __ge__(self, other):
+        return self.__ge__(other)
+
+    @tracing_not_supported
+    def __gt__(self, other):
+        return self.__gt__(other)
+
+    @tracing_not_supported
+    def __le__(self, other):
+        return self.__le__(other)
+
+    @tracing_not_supported
+    def __lt__(self, other):
+        return self.__lt__(other)
+
+    @tracing_not_supported
+    def ne(self, other):
+        return self.ne(other)
+
+    @tracing_not_supported
+    def __or__(self, other):
+        return self.__or__(other)
+
+    @tracing_not_supported
+    def __xor__(self, other):
+        return self.__xor__(other)
+
+    @tracing_not_supported
+    def __eq__(self, other):
+        return self.__eq__(other)
 
 
 class TracedFunction(object):
@@ -441,7 +614,7 @@ def trace(module: nn.Module, sample, depth=1000, basic_blocks=()):
 
     sample = [TracedValue(t, f"input{idx}") for idx, t in enumerate(sample)]
 
-    with patch_tensor_functions():
+    with patch_tensor_creating_functions():
         module(*sample)
 
     _unwrap_layers(module)
@@ -454,7 +627,7 @@ def trace(module: nn.Module, sample, depth=1000, basic_blocks=()):
 
 
 if __name__ == "__main__":
-    with patch_tensor_functions():
+    with patch_tensor_creating_functions():
         assert isinstance(torch.randn(10, 10), TracedValue)
 
         m = torch.as_tensor([[1, 2], [3, 4]])
@@ -501,12 +674,12 @@ if __name__ == "__main__":
         print(c.sum(dim=0))
 
     print()
-    print("all created scopes")
-    for s in DEBUG_CREATED_SCOPES:
-        print(s)
-    print()
-    sys.exit()
+    # sys.exit()
     m = resnet18()
     t = torch.randn(10, 3, 224, 224)
 
     trace(m, t, basic_blocks=(BasicBlock,))
+    print()
+    print(f"total created scopes: {len(DEBUG_CREATED_SCOPES)}")
+    for s in DEBUG_CREATED_SCOPES.values():
+        print(s)
