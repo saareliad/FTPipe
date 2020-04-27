3d82f01e78e319eed83a2810deb82c7f29deebe9
saareliad <saareliad@campus.technion.ac.il>
2020-04-27 19:07:58 +0300

    [WIP] Partitioning async pipeline

---
+++ partition_async_pipe.py
@@ -0,0 +1,97 @@
+import importlib
+from functools import partial
+from pytorch_Gpipe import PipelineConfig, pipe_model
+from pytorch_Gpipe.utils import layerDict, tensorDict
+
+
+def get_generated_last_stage_scopes(model, output_file, GET_PARTITIONS_ON_CPU=True):
+    module_path = output_file.replace("/", ".")
+    generated = importlib.import_module(module_path)
+    create_pipeline_configuration = generated.create_pipeline_configuration
+    config = create_pipeline_configuration(DEBUG=GET_PARTITIONS_ON_CPU)
+
+    n_stages = len(config['stages'])
+    last_stage = n_stages - 1
+    last_stage_cls = getattr(generated, f"Partition{last_stage}")
+
+    # TODO: this could be spared if the last partition would have scops as class attr
+    tensor_dict = tensorDict(model)
+    layer_dict = layerDict(
+        model, depth=config['depth'], basic_blocks=config['basic_blocks'])
+    last_stage_scopes = last_stage_cls(layer_dict, tensor_dict).scopes
+
+    return last_stage_scopes
+
+
+def force_no_recomp_fn_factory(scopes):
+    def f(scope):
+        return scope in scopes
+    return f
+
+
+class AsyncPipePartitioner:
+    def __init__(self, model, output_file, partition_method, *args, **kw):
+        """
+
+        Args:
+            original model (NOTE: this can be spared)
+            output_file of the generated model
+
+            partition_method, *args, **kw:
+                partitioning method + args and kw for it, execpt force_no_recomp_scopes.
+        """
+
+        if "force_no_recomp_scopes" in kw:
+            raise ValueError("force_no_recomp_scopes should be overriden")
+            # TODO: can implement composition...
+
+        self.partition_method = partial(partition_method, *args, **kw)
+        self.n_runs = 0
+        self.model = model
+        self.output_file = output_file
+
+    def partition(self, scopes_to_begin_with=set(), allowed_mistakes=2):
+        """Force no recomputation for given layers in last partition until a certain allowed_mistakes is achieved. """
+        self.last_partition_scopes = scopes_to_begin_with
+
+        current_mistakes = allowed_mistakes + 1
+
+        # TODO: take care of METIS seed here, can be important for stability
+
+        # Stats
+        stats = []
+
+        while current_mistakes > allowed_mistakes:
+
+            # Generate rule based on current scopes
+            f = force_no_recomp_fn_factory(self.last_partition_scopes)
+
+            # Partition
+            self.partition_method(force_no_recomp_scopes=f)
+            self.n_runs += 1
+
+            # Load last partition last stage scopes
+            generated_last_stage_scopes = generated_last_stage_scopes(
+                self.model, self.output_file, GET_PARTITIONS_ON_CPU=True)
+
+            # Count mistakes (false positives and false negatives)
+            A = set(last_partition_scopes)
+            B = set(generated_last_stage_scopes)
+            intersection = A & B
+            correct = len(intersection)
+            fp = len(A) - correct  # we predicted: true, result: false
+            fn = len(B) - correct  # we predicted: false, result: true
+            current_mistakes = fp + fn
+
+            # set current scopes as model scopes
+            self.last_partition_scopes = generated_last_stage_scopes
+
+            # stats:
+            d = dict(correct=correct, fp=fp,
+                     fn=fn, mistakes=current_mistakes)
+            stats.append(d)
+
+            # log something
+            print(f"run:{self.n_runs}", d)
+
+        return stats
