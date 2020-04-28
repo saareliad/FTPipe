73476b73336d07701feb8b8a225f503e702c55b4
Saar Eliad <saareliad@campus.technion.ac.il>
2020-04-28 15:32:23 +0300

    [WIP] partition_async pipe

--- partition_async_pipe.py
+++ partition_async_pipe.py
@@ -2,9 +2,12 @@ import importlib
 from functools import partial
 from pytorch_Gpipe import PipelineConfig, pipe_model
 from pytorch_Gpipe.utils import layerDict, tensorDict
+from collections import deque
 
 
-def get_generated_last_stage_scopes(model, output_file, GET_PARTITIONS_ON_CPU=True):
+def get_generated_last_stage_scopes(model,
+                                    output_file,
+                                    GET_PARTITIONS_ON_CPU=True):
     module_path = output_file.replace("/", ".")
     generated = importlib.import_module(module_path)
     create_pipeline_configuration = generated.create_pipeline_configuration
@@ -16,17 +19,19 @@ def get_generated_last_stage_scopes(model, output_file, GET_PARTITIONS_ON_CPU=Tr
 
     # TODO: this could be spared if the last partition would have scops as class attr
     tensor_dict = tensorDict(model)
-    layer_dict = layerDict(
-        model, depth=config['depth'], basic_blocks=config['basic_blocks'])
+    layer_dict = layerDict(model,
+                           depth=config['depth'],
+                           basic_blocks=config['basic_blocks'])
     last_stage_scopes = last_stage_cls(layer_dict, tensor_dict).scopes
 
     return last_stage_scopes
 
 
 def force_no_recomp_fn_factory(scopes):
-    def f(scope):
+    def foo(scope):
         return scope in scopes
-    return f
+
+    return foo
 
 
 class AsyncPipePartitioner:
@@ -45,14 +50,24 @@ class AsyncPipePartitioner:
             raise ValueError("force_no_recomp_scopes should be overriden")
             # TODO: can implement composition...
 
+        # TODO: can replace n_iter with a small number (e.g 1).
+
         self.partition_method = partial(partition_method, *args, **kw)
         self.n_runs = 0
         self.model = model
         self.output_file = output_file
 
-    def partition(self, scopes_to_begin_with=set(), allowed_mistakes=2):
+    def partition(self, force_no_recomp_scopes=None, scopes_to_begin_with=set(), allowed_mistakes=2):
         """Force no recomputation for given layers in last partition until a certain allowed_mistakes is achieved. """
-        self.last_partition_scopes = scopes_to_begin_with
+        
+        if force_no_recomp_scopes and scopes_to_begin_with:
+            raise ValueError("mutially exlusive")
+        elif force_no_recomp_scopes:
+            start_with_fn = True
+        else:
+            start_with_fn = False
+
+        last_partition_scopes = scopes_to_begin_with
 
         current_mistakes = allowed_mistakes + 1
 
@@ -61,17 +76,27 @@ class AsyncPipePartitioner:
         # Stats
         stats = []
 
+        # Results
+        # TODO: can recorded results limit with deque maxlen
+        scopes = []
+        scopes_arg = []
+        graphs = []
+
         while current_mistakes > allowed_mistakes:
 
             # Generate rule based on current scopes
-            f = force_no_recomp_fn_factory(self.last_partition_scopes)
+            if start_with_fn:
+                f = force_no_recomp_scopes
+                start_with_fn = False
+            else:
+                f = force_no_recomp_fn_factory(last_partition_scopes)
 
             # Partition
-            self.partition_method(force_no_recomp_scopes=f)
+            graph = self.partition_method(force_no_recomp_scopes=f)
             self.n_runs += 1
 
             # Load last partition last stage scopes
-            generated_last_stage_scopes = generated_last_stage_scopes(
+            generated_last_stage_scopes = get_generated_last_stage_scopes(
                 self.model, self.output_file, GET_PARTITIONS_ON_CPU=True)
 
             # Count mistakes (false positives and false negatives)
@@ -83,15 +108,22 @@ class AsyncPipePartitioner:
             fn = len(B) - correct  # we predicted: false, result: true
             current_mistakes = fp + fn
 
-            # set current scopes as model scopes
-            self.last_partition_scopes = generated_last_stage_scopes
-
             # stats:
-            d = dict(correct=correct, fp=fp,
-                     fn=fn, mistakes=current_mistakes)
+            d = dict(correct=correct, fp=fp, fn=fn, mistakes=current_mistakes)
             stats.append(d)
+            scopes.append(generated_last_stage_scopes)
+            scopes_arg.append(last_partition_scopes)
+            graphs.append(graph)
+
+            # set current scopes as model scopes
+            last_partition_scopes = generated_last_stage_scopes
 
             # log something
             print(f"run:{self.n_runs}", d)
 
-        return stats
+        # Save
+        self.stats = stats
+        self.scopes = scopes
+        self.graphs = graphs
+
+        return graph
