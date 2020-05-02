173f31a6e76b4bd8e9429683657b5ef1f7be63d5
alondj <alondej@gmail.com>
2020-05-02 15:43:42 +0300

    reintoduce Graph class

--- tracer.py
+++ tracer.py
@@ -4,6 +4,8 @@ from itertools import chain
 import operator
 import inspect
 from enum import IntEnum
+import pickle
+from typing import Tuple, Optional, Callable, Dict, Iterable
 
 import torch
 import torch.nn as nn
@@ -39,15 +41,14 @@ class NodeTypes(IntEnum):
         return self.name
 
 
-# TODO integrate with original Node
 class Node():
     def __init__(self, node_type, idx, scope):
         self.type = node_type
         self.id = idx
         self.scope = scope
 
-        self.stage_id = 0
-        self.profie = None
+        self.part = 0
+        self.profile = None
 
         self.out_edges = set()
         self.args = []
@@ -77,9 +78,372 @@ class Node():
         return list(chain(self.args, self.kwargs.keys()))
 
 
+GraphNodes = Dict[int, Node]
+NodeWeightFunction = Callable[[Node], int]
+EdgeWeightFunction = Callable[[Tuple[Node, Node]], int]
+
+
+class Graph():
+    def __init__(self, nodes: GraphNodes, output_id: int, depth: int, basic_blocks: Tuple[nn.Module, ...]):
+        self._nodes: GraphNodes = nodes
+        self.output_id = output_id
+        self.depth = depth
+        self.basic_blocks = basic_blocks
+
+    @property
+    def nodes(self) -> Iterable[Node]:
+        return self._nodes.values()
+
+    @property
+    def output_node(self) -> Node:
+        return self._nodes[self.output_id]
+
+    @property
+    def inputs(self):
+        return (n for n in self.nodes if n.type is NodeTypes.IN)
+
+    @property
+    def num_inputs(self) -> int:
+        return len(self.inputs)
+
+    @property
+    def num_partitions(self) -> int:
+        return len({n.part for n in self.nodes})
+
+    @property
+    def output_scopes(self):
+        return [self._nodes[self.output_id].scope]
+
+    @property
+    def outputs(self):
+        return [self._nodes[self.output_id]]
+
+    def asNetworkx(self,
+                   directed: bool = False,
+                   node_weight_function: Optional[NodeWeightFunction] = None,
+                   edge_weight_function: Optional[EdgeWeightFunction] = None):
+        '''
+        convert the graph into a weighted networkx graph.\n
+        each node will have a scope,partition idx and weight associated with it.\n
+        each weight will be weighted\n
+        graph can be directed or undirected for a directed graph weighting functions can be given
+        if not then weight will be set to 1.\n
+
+        Parameters:
+        ------------
+        directed:
+            whether to return a directed graph default is undirected
+        node_weight_function:
+            an optional weight function for the nodes should be a function from Node to int
+            if not given a default weight of 1 will be given to all nodes
+        edge_weight_function:
+            an optional weight function for the edges should be a function (Node,Node) to int
+            if not given a default value of 1 will be given to all edges
+        '''
+        try:
+            import networkx as nx
+        except ImportError:
+            print("networkx package not found")
+            return
+
+        if directed:
+            G = nx.DiGraph()
+        else:
+            G = nx.Graph()
+
+        for u in self.nodes:
+            for v in u.out_nodes:
+                if edge_weight_function is None:
+                    w = 1
+                else:
+                    w = edge_weight_function(u, v)
+                G.add_edge(u.id, v.id, weight=w)
+
+        for n in self.nodes:
+            if node_weight_function is None:
+                w = 1
+            else:
+                w = node_weight_function(n)
+            G.nodes[n.id]['weight'] = w
+
+        return G
+
+    def build_dot(self,
+                  show_buffs_params: bool = True,
+                  show_profiles: bool = True,
+                  edge_weight_function=None):
+        '''
+        return a graphviz representation of the graph
+        Parameters
+        ----------
+        show_buffs_params:
+            whether to display also buffers and parameters which are not encased in the graph scopes
+        show_profiles:
+            whether to display the nodes weight
+        edge_weight_function:
+            function to get edge weights
+        '''
+        theme = {
+            "background_color": "#FFFFFF",
+            "fill_color": "#E8E8E8",
+            "outline_color": "#000000",
+            "font_color": "#000000",
+            "font_name": "Times",
+            "font_size": "10",
+            "margin": "0,0",
+            "padding": "1.0,0.5"
+        }
+        from graphviz import Digraph
+
+        dot = Digraph()
+        dot.attr("graph",
+                 concentrate="true",
+                 bgcolor=theme["background_color"],
+                 color=theme["outline_color"],
+                 fontsize=theme["font_size"],
+                 fontcolor=theme["font_color"],
+                 fontname=theme["font_name"],
+                 margin=theme["margin"],
+                 rankdir="TB",
+                 pad=theme["padding"])
+
+        dot.attr("node",
+                 shape="box",
+                 style="filled",
+                 margin="0,0",
+                 fillcolor=theme["fill_color"],
+                 color=theme["outline_color"],
+                 fontsize=theme["font_size"],
+                 fontcolor=theme["font_color"],
+                 fontname=theme["font_name"])
+
+        dot.attr("edge",
+                 style="solid",
+                 color=theme["outline_color"],
+                 fontsize=theme["font_size"],
+                 fontcolor=theme["font_color"],
+                 fontname=theme["font_name"])
+
+        # TODO split big graphs to multiple pdfs
+
+        colors = {
+            0: 'grey',
+            1: 'green',
+            2: 'red',
+            3: 'yellow',
+            4: 'orange',
+            5: 'brown',
+            6: 'purple',
+            7: 'pink',
+            8: 'cyan',
+            9: 'gold',
+            10: 'darkolivegreen',
+            11: 'seagreen',
+            12: 'thistle',
+            13: 'plum',
+            14: 'deeppink',
+            15: 'lightyellow',
+            16: 'tan'
+        }
+
+        def predicate(n):
+            return (n.type != NodeTypes.BUFF_PARAM) or show_buffs_params
+
+        # add nodes
+        for node in self.nodes:
+            node_id = node.id
+            if predicate(node):
+                scope = node.scope
+                value_type = node.value_type
+                node_label = f"{scope}\nidx: {node_id}\nvalue type: {value_type}"
+                if node_id == self.output_id:
+                    node_label += "\nmodel output"
+                if node.type is NodeTypes.IN:
+                    node_label += "\nmodel input"
+                if node.type is NodeTypes.CONSTANT:
+                    node_label += f"\nvalue: {node.constant_value}"
+
+                if issubclass(node.value_type, Tensor):
+                    node_label += f"\ntensor of type: {node.tensor_dtype}\nshape: {node.tensor_shape}"
+
+                if show_profiles and node.profile:
+                    node_label = f"{node_label}\nProfile:"
+                    for k, v in node.weight._asdict().items():
+                        node_label += f"\n{k}:{v}"
+                        if "time" in k:
+                            node_label += " ms"
+                        elif "memory" in k or "size" in k:
+                            node_label += " MB"
+
+                dot.node(str(node_id), label=node_label,
+                         fillcolor=colors[node.part])
+
+                # add edges
+                args, kwargs = node.args, node.kwargs
+                for idx, i in enumerate(args):
+                    if predicate(i):
+                        edge_label = f"arg: {idx}"
+                        if edge_weight_function:
+                            edge_label += f"\nweight: {edge_weight_function(i,node)}"
+                        dot.edge(str(i.id), str(node_id), label=edge_label)
+
+                for i, kw in kwargs.items():
+                    if predicate(i):
+                        edge_label = f"kwarg: {kw}"
+                        if edge_weight_function:
+                            edge_label += f"\nweight: {edge_weight_function(i,node)}"
+                        dot.edge(str(i.id), str(node_id), label=edge_label)
+
+        return dot
+
+    def display(self,
+                show_buffs_params: bool = True,
+                show_profiles: bool = True,
+                edge_weight_function=None):
+        '''
+        display the graph in Jupyter
+
+        Parameters
+        ----------
+        show_buffs_params:
+            whether to display also buffers and parameters which are not encased in the graph scopes
+        show_profiles:
+            whether to display the nodes weight
+        edge_weight_function:
+            edge weight function to use
+        '''
+        try:
+            from IPython.core.display import display_svg
+            display_svg(self.build_dot(show_buffs_params,
+                                       show_profiles=show_profiles,
+                                       edge_weight_function=edge_weight_function),
+                        raw=False)
+        except ImportError:
+            print("only works in ipython notebooks")
+
+    def save_as_pdf(self,
+                    file_name: str,
+                    directory: str,
+                    show_buffs_params: bool = True,
+                    show_profiles: bool = True,
+                    edge_weight_function=None):
+        '''
+        save the rendered graph to a pdf file
+
+        Parameters
+        ----------
+        file_name:
+            the name of the saved file
+        directory:
+            directory to store the file in
+        show_buffs_params:
+            whether to display also buffers and parameters which are not encased in the graph scopes
+        show_profiles:
+            whether to display the nodes weight
+        '''
+        dot = self.build_dot(show_buffs_params, show_profiles=show_profiles,
+                             edge_weight_function=edge_weight_function)
+        dot.format = "pdf"
+        import os
+        if os.path.exists(f"{directory}/{file_name}.pdf"):
+            os.remove(f"{directory}/{file_name}.pdf")
+        dot.render(file_name, directory=directory, cleanup=True)
+        return self
+
+    def serialize(self, path: str):
+        '''
+        serializes the graph to the given path
+        can later be restored using Graph.deserialize(path)
+
+        Parameters:
+        -----------
+        path:
+            the path to store the graph object file will be called path.graph
+        '''
+        if not path.endswith(".graph"):
+            path += ".graph"
+
+        pickle.dump(self.state(), open(path, "wb"))
+
+    def state(self):
+        '''
+        returns a dicitionary containing the graphs state
+        '''
+
+        node_states = []
+        for node in self.nodes:
+            state = dict(id=node.id, scope=node.scope, type=node.type,
+                         part=node.part, profile=node.profile,
+                         out_edges=[n.id for n in node.out_edges],
+                         args=[n.id for n in node.args],
+                         kwargs={n.id: kw for n,
+                                 kw in node.kwargs.items()},
+                         value_type=node.value_type,
+                         constant_value=node.constant_value,
+                         tensor_dtype=node.tensor_dtype,
+                         tensor_shape=node.tensor_shape)
+            node_states.append(state)
+
+        return{"node_data": node_states,
+               "output_id": self.output_id,
+               "depth": self.depth,
+               "basic_blocks": self.basic_blocks
+               }
+
+    @classmethod
+    def deserialize(cls, path: str) -> "Graph":
+        '''
+        deserializes the graph from the path returning a Graph object
+
+        Parameters:
+        -------------
+        path:
+        the path to where the graph is stored
+        '''
+        if not path.endswith(".graph"):
+            path += ".graph"
+
+        graph_data = pickle.load(open(path, "rb"))
+        output_id = graph_data['output_id']
+        depth = graph_data['depth']
+        basic_blocks = graph_data['basic_blocks']
+
+        nodes = dict()
+
+        states = graph_data['node_data']
+        for state in states:
+            node = Node(state['type'], state['id'], state['scope'])
+            nodes[node.id] = node
+
+            node.part = state['part']
+            node.profile = state['profile']
+            node.args = [nodes[n] for n in state['args']]
+            node.kwargs = {nodes[n]: kw for n, kw in state['kwargs'].items()}
+            node.constant_value = state['constant_value']
+            node.value_type = state['value_type']
+            node.tensor_dtype = state['tensor_dtype']
+            node.tensor_shape = state['tensor_shape']
+
+        for node in nodes.values():
+            node.out_edges = {nodes[n] for n in states[node.id]['out_edges']}
+
+        return cls(nodes, output_id, depth, basic_blocks)
+
+    def layers_graph(self) -> Tuple["Graph", Dict[int, int]]:
+        '''
+        creates a graph g with nodes of types OP PYTHON_PRIMITIVE and CONSTANT removed
+        leaving only inputs layers and params/buffers
+
+        returns the created graph and a map between g's indices and self indices
+        '''
+        raise NotImplementedError()
+
+
 ##############################
 # Tracing Wrappers
 ##############################
+
+
 class TracedFunctions():
     functions = set()
 
@@ -564,7 +928,7 @@ def trace(module: nn.Module, args=(), kwargs=None, depth=1000, basic_blocks=()):
     if not is_valid:
         raise RuntimeError(errors)
 
-    return nodes, output_id
+    return Graph(nodes, output_id, depth, basic_blocks)
 
 
 def prepare_args_and_kwargs(args=(), kwargs=None):
@@ -908,84 +1272,6 @@ def container_construct_op_name(container_cls):
 
 
 ##############################
-# Graph visualization
-##############################
-def show_graph(nodes, output_id, filename="graph"):
-    build_dot(nodes, output_id).render(filename=filename,
-                                       directory='.', cleanup=True, format='pdf')
-
-
-def build_dot(nodes, output_id):
-    theme = {
-        "background_color": "#FFFFFF",
-        "fill_color": "#E8E8E8",
-        "outline_color": "#000000",
-        "font_color": "#000000",
-        "font_name": "Times",
-        "font_size": "10",
-        "margin": "0,0",
-        "padding": "1.0,0.5"
-    }
-    from graphviz import Digraph
-
-    dot = Digraph()
-    dot.attr("graph",
-             concentrate="true",
-             bgcolor=theme["background_color"],
-             color=theme["outline_color"],
-             fontsize=theme["font_size"],
-             fontcolor=theme["font_color"],
-             fontname=theme["font_name"],
-             margin=theme["margin"],
-             rankdir="TB",
-             pad=theme["padding"])
-
-    dot.attr("node",
-             shape="box",
-             style="filled",
-             margin="0,0",
-             fillcolor=theme["fill_color"],
-             color=theme["outline_color"],
-             fontsize=theme["font_size"],
-             fontcolor=theme["font_color"],
-             fontname=theme["font_name"])
-
-    dot.attr("edge",
-             style="solid",
-             color=theme["outline_color"],
-             fontsize=theme["font_size"],
-             fontcolor=theme["font_color"],
-             fontname=theme["font_name"])
-
-    # add nodes
-    for node_id in range(len(nodes)):
-        node = nodes[node_id]
-        scope = node.scope
-        value_type = node.value_type
-        node_label = f"{scope}\nidx: {node_id}\nvalue type: {value_type}"
-        if node_id == output_id:
-            node_label += "\nmodel output"
-        if node.type is NodeTypes.IN:
-            node_label += "\nmodel input"
-        if node.type is NodeTypes.CONSTANT:
-            node_label += f"\nvalue: {node.constant_value}"
-
-        if issubclass(node.value_type, Tensor):
-            node_label += f"\ntensor of type: {node.tensor_dtype}\nshape: {node.tensor_shape}"
-
-        dot.node(str(node_id), label=node_label, fillcolor="grey")
-
-        # add edges
-        args, kwargs = node.args, node.kwargs
-        for idx, i in enumerate(args):
-            dot.edge(str(i.id), str(node_id), label=f"arg: {idx}")
-        for i, kw in kwargs.items():
-            dot.edge(str(i.id), str(node_id), label=f"kwarg: {kw}")
-
-    return dot
-
-
-##############################
 # check that the graph is valid
 # ensure all recorded data is consistent
 ##############################
@@ -1143,7 +1429,7 @@ inplace_arithmetic_ops = {"__iadd__": " +=",
                           "__ipow__": "**="}
 
 
-def compile_model(nodes, output_id, output_file=None):
+def compile_model(graph, output_file=None):
     statements = []
     ready_expressions = dict()
 
@@ -1152,8 +1438,8 @@ def compile_model(nodes, output_id, output_file=None):
     namespaces = {namespace.__name__ for namespace in
                   chain(get_overridable_functions().keys(), TracedFunctions.traced_namespaces())}
 
-    for idx in range(len(nodes)):
-        node = nodes[idx]
+    for node in graph.nodes:
+        idx = node.id
         scope = node.scope
         node_type = node.type
         if node_type is NodeTypes.IN:
@@ -1263,7 +1549,7 @@ def compile_model(nodes, output_id, output_file=None):
                         f"t_{idx} = {self_arg}.{func_name}({', '.join(param_list[1:])})")
             ready_expressions[node] = f"t_{idx}"
 
-    statements.append(f"return {ready_expressions[nodes[output_id]]}")
+    statements.append(f"return {ready_expressions[graph.output_node]}")
     print("\n")
     stage_input_str = ", ".join(["self"] + model_args + model_kwargs)
 
@@ -1319,7 +1605,6 @@ def set_node_indices(nodes, output_id):
 
     new_nodes = dict()
 
-    # populate lookup table for new ids
     for idx, node in enumerate(nodes.values()):
         assert idx <= node.id
 
@@ -1452,12 +1737,11 @@ def test_cases():
         kwargs = dict()
         for d in range(3):
             print(f"depth_{d}")
-            nodes, output_id = trace(m, depth=d, args=args, kwargs=kwargs)
+            graph = trace(m, depth=d, args=args, kwargs=kwargs)
             print()
-            compile_model(nodes, output_id,
+            compile_model(graph,
                           output_file=f"{type(m).__name__}_depth_{d}")
-            show_graph(nodes, output_id,
-                       filename=f"{type(m).__name__}_depth_{d}")
+            graph.save_as_pdf(f"{type(m).__name__}_depth_{d}", ".")
 
 
 if __name__ == "__main__":
