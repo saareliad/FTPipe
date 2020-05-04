e1d8123621734a7e844c8512c414f0307ca45044
Saar Eliad <saareliad@campus.technion.ac.il>
2020-05-04 15:23:39 +0300

    Getting automatic names in file. Renaming config

--- models/transformers_cfg.py
+++ models/transformers_cfg.py
@@ -4,6 +4,10 @@
 from transformers import (GPT2Config, GPT2Tokenizer)
 from .normal import GPT2LMHeadModel, GPT2Model, StatelessGPT2LMHeadModel
 
+import sys
+from inspect import getmembers, isfunction
+
+
 # We use this to get cfg_class, model_class, and tokenizer_class
 MODEL_TYPES = {
     'gpt2': (GPT2Config, GPT2Model, GPT2Tokenizer),
@@ -14,7 +18,15 @@ MODEL_TYPES = {
 # NOTE: some of these configs are just for this repo, see
 # `models.transformers_utils.pretrained_model_config_and_tokenizer`
 
-def gpt2_tied_lm_5p():
+def gpt2_4p_untied():
+    return dict(model_type='gpt2_lm_stateless',
+                model_name_or_path='gpt2',
+                do_lower_case=False,
+                output_past=False,
+                stateless_tied=False)
+
+
+def gpt2_4p_tied():
     return dict(model_type='gpt2_lm_stateless',
                 model_name_or_path='gpt2',
                 do_lower_case=False,
@@ -28,9 +40,20 @@ def gpt2xl_8p_untied():
                 do_lower_case=False,
                 output_past=False,
                 stateless_tied=False)
-    
 
-MODEL_TOKENIZER_AND_CONFIG_FUNCTIONS = {
-    'gpt2_tied_lm_5p': gpt2_tied_lm_5p,
-    'gpt2xl_8p_untied': gpt2xl_8p_untied,
-}
+
+def gpt2xl_8p_tied():
+    return dict(model_type='gpt2_lm_stateless',
+                model_name_or_path='gpt2-xl',
+                do_lower_case=False,
+                output_past=False,
+                stateless_tied=True)
+
+
+functions_list = getmembers(
+    sys.modules[__name__],
+    lambda o: isfunction(o) and o.__module__ == __name__)
+
+# dict of all functions in current file.
+# name --> function
+MODEL_TOKENIZER_AND_CONFIG_FUNCTIONS = {i: v for i, v in functions_list}
