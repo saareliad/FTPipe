ffeed9427bc08d9f047702f26fcb2cf65020d21c
Saar Eliad <saareliad@campus.technion.ac.il>
2020-05-06 23:43:42 +0300

    docker

--- trace_feature/modified_sources/run_docker.sh
+++ trace_feature/modified_sources/run_docker.sh
@@ -2,4 +2,4 @@
 # may change image name.
 # TODO: share cache for models/datasets
 # docker run -it --ipc=host --rm -v $(pwd):/workspace partitioning/live
-docker run -it --ipc=host --rm -p 12345:12345 -v $(pwd):/workspace:Z partitioning/live
+docker run -it --ipc=host --rm -p 12345:12345 -v $(pwd):/workspace:z partitioning/live
