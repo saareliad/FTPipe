9c84dd931d8581b6dd0b678ec184841de04d681b
Saar Eliad <saareliad@campus.technion.ac.il>
2020-05-21 16:25:30 +0300

    test rng stasher... works

--- pipeline/rng_stasher.py
+++ pipeline/rng_stasher.py
@@ -12,7 +12,6 @@ class PartitionRngStasher:
     #   (1) it will be problematic when 2 recomputing stages are use the same device. (e.g tied GPipe)
     #   (2) currently does not set numpy or python random seeds. just pytorch's. (TODO)
     """
-
     def __init__(self, device=torch.device('cpu')):
         self.device = device
         self.state = {}
@@ -39,3 +38,33 @@ class PartitionRngStasher:
 
     def clear_state(self):
         self.state.clear()
+
+
+if __name__ == "__main__":
+    import torch
+    import torch.nn as nn
+
+    # CPU
+    model = nn.Sequential(nn.Linear(10, 10), nn.Dropout2d(0.5))
+    s = PartitionRngStasher()
+    s.stash_rng_state(0)
+    a = model(torch.ones(4, 10))
+    b = model(torch.ones(4, 10))
+    assert not torch.allclose(a, b)
+    with torch.random.fork_rng(devices=s.devices):
+        s.restore_rng_state(0)
+        c = model(torch.ones(4, 10))
+    assert torch.allclose(a, c)
+
+    # Cuda
+    model = nn.Sequential(nn.Linear(10, 10), nn.Dropout2d(0.5))
+    device = torch.device("cuda:0")
+    x = torch.ones(4, 10, device=device)
+    model = model.to(device)
+    s = PartitionRngStasher(device=device)
+    s.stash_rng_state(0)
+    a = model(x)
+    with torch.random.fork_rng(devices=s.devices):
+        s.restore_rng_state(0)
+        b = model(x)
+    torch.allclose(a, b)
