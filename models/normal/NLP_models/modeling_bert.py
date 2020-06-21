7bffb3f75eb935657f5f9ed7a6945099c1e530cc
Saar Eliad <saareliad@campus.technion.ac.il>
2020-06-21 11:57:00 +0300

    bert for glue with decoupled loss. Should handle both regression and classification tasks.

--- models/normal/NLP_models/modeling_bert.py
+++ models/normal/NLP_models/modeling_bert.py
@@ -1173,40 +1173,56 @@ class BertForSequenceClassification(BertPreTrainedModel):
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.classifier = nn.Linear(config.hidden_size, self.config.num_labels)
 
+        # if self.num_labels == 1:
+        #     self.loss = nn.MSELoss()
+        # else:
+        #     self.loss = nn.CrossEntropyLoss()
+
+        self.init_weights()
+
+    def forward(self, input_ids, attention_mask, token_type_ids,
+                ):
+
+        outputs = self.bert(input_ids,
+                            attention_mask=attention_mask,
+                            token_type_ids=token_type_ids,
+                            )
+        pooled_output = outputs[1]
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+        return logits
+        # # add hidden states and attention if they are here
+        # outputs = (logits,)
+
+        # if is_not_None(labels):# is not None:
+        #     if self.num_labels == 1:
+        #         #  We are doing regression
+        #         loss = self.loss(logits.view(-1), labels.view(-1))
+        #     else:
+        #         loss = self.loss(
+        #             logits.view(-1, self.num_labels), labels.view(-1))
+        #     outputs = (loss,)
+
+        # return outputs  # loss, logits,
+
+
+class GlueLoss(torch.nn.Module):
+    def __init__(self, num_labels):  # config.num_labels
+        super().__init__()
+        self.num_labels = num_labels
         if self.num_labels == 1:
             self.loss = nn.MSELoss()
         else:
             self.loss = nn.CrossEntropyLoss()
 
-        self.init_weights()
-
-    def forward(self, input_ids, labels=None, attention_mask=None, token_type_ids=None,
-                position_ids=None, head_mask=None):
-
-        outputs = self.bert(input_ids,
-                            attention_mask=attention_mask,
-                            token_type_ids=token_type_ids,
-                            position_ids=position_ids,
-                            head_mask=head_mask)
-
-        pooled_output = outputs[1]
-
-        pooled_output = self.dropout(pooled_output)
-        logits = self.classifier(pooled_output)
-
-        # add hidden states and attention if they are here
-        outputs = (logits,)
-
-        if is_not_None(labels):# is not None:
-            if self.num_labels == 1:
-                #  We are doing regression
-                loss = self.loss(logits.view(-1), labels.view(-1))
-            else:
-                loss = self.loss(
-                    logits.view(-1, self.num_labels), labels.view(-1))
-            outputs = (loss,)
-
-        return outputs  # loss, logits, 
+    def forward(self, logits, labels):
+        if self.num_labels == 1:
+            #  We are doing regression
+            loss = self.loss(logits.view(-1), labels.view(-1))
+        else:
+            loss = self.loss(
+                logits.view(-1, self.num_labels), labels.view(-1))
+        return loss
 
 
 @add_start_docstrings("""Bert Model with a multiple choice classification head on top (a linear layer on top of
