c396ee417654cca7592af8828913135ffdc2b1bb
Saar Eliad <saareliad@campus.technion.ac.il>
2020-06-24 15:54:52 +0300

    [wip] added squad1 dataset (requires pip install nlp)

--- partition_T5_models.py
+++ partition_T5_models.py
@@ -14,6 +14,14 @@ import functools
 from partition_async_pipe import partition_async_pipe
 from partition_scripts_utils import choose_blocks, ParseAcyclicPartitionerOpts, ParseMetisOpts, ParsePartitioningOpts, record_cmdline
 from misc import run_analysis, run_partitions
+import nlp
+import dataclasses
+from dataclasses import dataclass, field
+from typing import Dict, List, Optional
+import numpy as np
+import torch
+from transformers import (
+    DataCollator, )
 
 # See https://huggingface.co/models
 T5_PRETRAINED_MODELS = {'t5-small', 't5-base', 't5-large', 't5-3b', 't5-11b'}
@@ -52,15 +60,6 @@ def get_model_and_tokenizer(args):
     return model, tokenizer
 
 
-def get_input(args, tokenizer, analysis=False):
-
-    T5_TASK_TO_GET_INPUT = {
-        "dummy": get_input_dummy,
-    }
-    print("-I- geeting input for t5_task: {args.t5_task}")
-    return T5_TASK_TO_GET_INPUT[args.t5_task](args, tokenizer, analysis)
-
-
 def get_input_dummy(args, tokenizer, analysis=False):
     input_ids = tokenizer.encode("Hello, my dog is cute",
                                  return_tensors="pt").to(
@@ -88,6 +87,125 @@ def get_input_dummy(args, tokenizer, analysis=False):
     return kwargs
 
 
+def get_input_squad1(args, tokenizer, analysis=False):
+    # see https://colab.research.google.com/github/patil-suraj/exploring-T5/blob/master/T5_on_TPU.ipynb#scrollTo=2ZWE4addfSmi
+    batch_size = args.analysis_batch_size if analysis else args.partitioning_batch_size
+
+    #########################
+    # Yucky squad stuff
+    #########################
+
+    # process the examples in input and target text format and the eos token at the end
+    def add_eos_to_examples(example):
+        example['input_text'] = 'question: %s  context: %s </s>' % (
+            example['question'], example['context'])
+        example['target_text'] = '%s </s>' % example['answers']['text'][0]
+        return example
+
+    # tokenize the examples
+    # NOTE: they use global tokenizer
+    def convert_to_features(example_batch):
+        input_encodings = tokenizer.batch_encode_plus(
+            example_batch['input_text'],
+            pad_to_max_length=True,
+            max_length=512
+        )  # NOTE: I think this could be changed to 384 like bert to save memory.
+        target_encodings = tokenizer.batch_encode_plus(
+            example_batch['target_text'],
+            pad_to_max_length=True,
+            max_length=16)
+
+        encodings = {
+            'input_ids': input_encodings['input_ids'],
+            'attention_mask': input_encodings['attention_mask'],
+            'target_ids': target_encodings['input_ids'],
+            'target_attention_mask': target_encodings['attention_mask']
+        }
+
+    # load train and validation split of squad
+    train_dataset = nlp.load_dataset('squad', split=nlp.Split.TRAIN)
+    # valid_dataset = nlp.load_dataset('squad', split=nlp.Split.VALIDATION)
+
+    # map add_eos_to_examples function to the dataset example wise
+    train_dataset = train_dataset.map(add_eos_to_examples)
+    # map convert_to_features batch wise
+    train_dataset = train_dataset.map(convert_to_features, batched=True)
+
+    # valid_dataset = valid_dataset.map(add_eos_to_examples,
+    #                                   load_from_cache_file=False)
+    # valid_dataset = valid_dataset.map(convert_to_features,
+    #                                   batched=True,
+    #                                   load_from_cache_file=False)
+
+    # set the tensor type and the columns which the dataset should return
+    columns = [
+        'input_ids', 'target_ids', 'attention_mask', 'target_attention_mask'
+    ]
+    train_dataset.set_format(type='torch', columns=columns)
+    # valid_dataset.set_format(type='torch', columns=columns)
+
+    # prepares lm_labels from target_ids, returns examples with keys as expected by the forward method
+    # this is necessacry because the trainer directly passes this dict as arguments to the model
+    # so make sure the keys match the parameter names of the forward method
+
+    # NOTE: slightly changed becase we want to pin memory and huggingface don't do it
+    @dataclass
+    class T2TDataCollator(DataCollator):
+        def __init__(self, batch):
+            self.batch = self.collate_batch(batch)
+
+        def collate_batch(self, batch: List) -> Dict[str, torch.Tensor]:
+            """
+            Take a list of samples from a Dataset and collate them into a batch.
+            Returns:
+                A dictionary of tensors
+            """
+            input_ids = torch.stack(
+                [example['input_ids'] for example in batch])
+            lm_labels = torch.stack(
+                [example['target_ids'] for example in batch])
+            lm_labels[lm_labels[:, :] == 0] = -100
+            attention_mask = torch.stack(
+                [example['attention_mask'] for example in batch])
+            decoder_attention_mask = torch.stack(
+                [example['target_attention_mask'] for example in batch])
+
+            return {
+                'input_ids': input_ids,
+                'attention_mask': attention_mask,
+                'lm_labels': lm_labels,
+                'decoder_attention_mask': decoder_attention_mask
+            }
+
+        def pin_memory(self):
+            for v in self.batch.values():
+                v.pin_memory()
+
+            return self
+
+    collate_fn = T2TDataCollator().collate_batch
+    dl = torch.utils.data.Dataloader(dataset=train_dataset,
+                                     shuffle=True,
+                                     batch_size=batch_size,
+                                     collate_fn=collate_fn,
+                                     pin_memory=True)
+
+    batch = next(dl)
+    return batch
+
+
+T5_TASK_TO_GET_INPUT = {
+    "dummy": get_input_dummy,
+    'squad1': get_input_squad1,
+}
+
+
+def get_input(args, tokenizer, analysis=False):
+
+    print("-I- geeting input for t5_task: {args.t5_task}")
+    return T5_TASK_TO_GET_INPUT[args.t5_task](args, tokenizer, analysis)
+
+
 class ParsePartitioningT5Opts(ParsePartitioningOpts):
     def _extra(self, parser):
         parser.add_argument("--model",
@@ -100,7 +218,7 @@ class ParsePartitioningT5Opts(ParsePartitioningOpts):
 
         parser.add_argument("--t5_task",
                             type=str,
-                            choices=["dummy"],
+                            choices=T5_TASK_TO_GET_INPUT.keys(),
                             default="dummy")
 
     def set_defaults(self, parser):
@@ -129,7 +247,7 @@ def parse_cli():
     args = parser.parse_args()
     args.auto_file_name = not args.no_auto_file_name
     if args.auto_file_name:
-        args.output_file = f"{args.model.replace('-','_')}_p{args.n_partitions}"
+        args.output_file = f"{args.model.replace('-','_')}_p{args.n_partitions}_{args.t5_task}"
 
     if args.output_file.endswith(".py"):
         args.output_file = args.output_file[:-3]
@@ -144,6 +262,8 @@ def parse_cli():
     args.device = device
 
     return args
+
+    return encodings
 
 
 if __name__ == "__main__":
