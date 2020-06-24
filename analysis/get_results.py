6e4405fd6c3674397cb93dc239fbc0cda7103d07
Saar Eliad <saareliad@campus.technion.ac.il>
2020-06-24 15:02:18 +0300

    added: wieght stashing and lm tasks

--- analysis/get_results.py
+++ analysis/get_results.py
@@ -11,6 +11,9 @@ def alg(fn):
         ga_alg = f"{a}_ws_ga"
         if ga_alg in fn:
             return ga_alg
+        ws_alg = "{a}_ws"
+        if ws_alg+"_" in fn:
+            return ws_alg
 
     return "gpipe" if "gpipe" in fn else "aggmsnag" if "aggmsnag" in fn else "msnag" if "msnag" in fn else "stale" if "stale" in fn else "BUG"
 
@@ -26,6 +29,12 @@ def read_desc_df(path="desc.csv"):
 def filter_desc_df_squad(desc):
     df = desc
     return df[[(i, j) for i in ['f1', 'em', 'total_time']
+               for j in ['mean', 'max', 'min', 'std']]]
+
+
+def filter_desc_df_lm(desc):
+    df = desc
+    return df[[(i, j) for i in ['ppl', 'total_time']
                for j in ['mean', 'max', 'min', 'std']]]
 
 
@@ -60,10 +69,40 @@ def write_squad_desc_df():
     df.to_csv("df.csv", index=False)
 
 
+def write_lm_desc_df():
+    ls = glob.glob
+    records = []
+    for f in ls("*.json"):
+        d = {}
+        with open(f, "rb") as fd:
+            r = json.load(fd)
+            d['name'] = f
+            d['alg'] = alg(f)
+            d['seed'] = r['config']['seed']
+            d['agg'] = r['config']['step_every']
+            agg = d['agg']
+            mb = r['config']['bs_train']
+            d['batch'] = agg * mb
+            d['total_time'] = r['config']['exp_total_time']
+            d['ppl'] = r['results']['test_ppl'][-1]
+
+        records.append(d)
+
+    df = pd.DataFrame.from_records(records)
+    print(df)
+    desc = df.groupby(['alg', 'batch', 'agg']).describe()
+    desc = desc[['ppl', 'total_time']]
+    print(desc)
+
+    # TODO: to csv with headers
+    desc.to_csv("desc.csv", index=True)
+    df.to_csv("df.csv", index=False)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Squad results analysis')
     parser.add_argument("--task",
-                        choices=["squad"],
+                        choices=["squad", "lm"],
                         default="squad",
                         help="Learning Task")
 
@@ -73,13 +112,13 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    WRITE = {'squad': write_squad_desc_df}
-    FILTER = {'squad': filter_desc_df_squad}
+    WRITE = {'squad': write_squad_desc_df, 'lm': write_lm_desc_df}
+    FILTER = {'squad': filter_desc_df_squad, 'lm': filter_desc_df_lm}
 
 
     if args.write:
         WRITE[args.task]()
-    
+
     if args.read:
         desc = read_desc_df()
         if args.filter:
