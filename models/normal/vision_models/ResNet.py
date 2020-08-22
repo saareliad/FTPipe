3ec71f31b07aed03f8e98e292f21a9d99b4bac09
alondj <alondej@gmail.com>
2020-08-22 10:14:30 +0300

    [models] removed auto import of torchvision

--- models/normal/vision_models/ResNet.py
+++ models/normal/vision_models/ResNet.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torchvision.models.utils import load_state_dict_from_url
 
 
 __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
@@ -212,6 +211,7 @@ class ResNet(nn.Module):
 def _resnet(arch, block, layers, pretrained, progress, **kwargs):
     model = ResNet(block, layers, **kwargs)
     if pretrained:
+        from torchvision.models.utils import load_state_dict_from_url
         state_dict = load_state_dict_from_url(model_urls[arch],
                                               progress=progress)
         model.load_state_dict(state_dict)
