6af75732a0aa07fd50f959f289c5fce9415966c0
saareliad <saareliad@campus.technion.ac.il>
2020-08-25 11:02:27 +0300

    Removed duplicate code

--- models/transformers_utils.py
+++ models/transformers_utils.py
@@ -1,52 +1,11 @@
-from .transformers_cfg import MODEL_TOKENIZER_AND_CONFIG_FUNCTIONS, MODEL_TYPES
-from .models import CommonModelHandler
-from transformers import AutoModel, AutoConfig, AutoTokenizer
 from enum import Enum, auto
-import os
+from .transformers_cfg import MODEL_TYPES
 
 
 class GetConfigFrom(Enum):
     HardCoded = auto()
     ParsedArgs = auto()
     Generated = auto()
-
-
-class HFModelHandler(CommonModelHandler):
-    def __init__(self,
-                 method: GetConfigFrom = GetConfigFrom.HardCoded,
-                 *args, **kw):
-        super().__init__(*args, **kw)
-        self.pipeline_transformer_config = None
-        self.method = method
-
-        self.tokenizer = None
-        self.config = None
-
-    def get_normal_model_instance(self, *args, **kw):
-        cfg = self.get_pipeline_transformer_config()
-        model, tokenizer, config = pretrained_model_config_and_tokenizer(**cfg)
-
-        self.tokenizer = tokenizer
-        self.config = config
-        return model
-
-    def get_pipeline_transformer_config(self):
-        if self.pipeline_transformer_config is None:
-            if self.method == GetConfigFrom.Generated:
-                raise NotImplementedError()
-            elif self.method == GetConfigFrom.ParsedArgs:
-                raise NotImplementedError()
-            elif self.method == GetConfigFrom.HardCoded:
-                assert not os.path.exists(self.generated_file_name_or_path)
-                cfg = MODEL_TOKENIZER_AND_CONFIG_FUNCTIONS.get(self.generated_file_name_or_path)()
-            self.pipeline_transformer_config = cfg
-        return self.pipeline_transformer_config
-
-    def get_extra(self, *args, **kw):
-        return dict(config=self.config, tokenizer=self.tokenizer)
-
-    def get_loader(self, *args, **kw):
-        raise NotImplementedError()
 
 
 def resize_token_embeddings(model, tokenizer):
@@ -66,6 +25,7 @@ def pretrained_model_config_and_tokenizer(
         explicitly_set_dict={},
         **config_kw
 ):
+    # NOTE its not AutoModel because we sometimes slightly modify the model.
     config_class, model_class, tokenizer_class = MODEL_TYPES[model_type]
     config = config_class.from_pretrained(
         config_name if config_name else model_name_or_path,
