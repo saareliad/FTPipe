47e74ca43547561634e1d58721f91adbb0085e6b
saareliad <saareliad@campus.technion.ac.il>
2020-09-01 09:36:36 +0300

    added analysis for padding
    (to assert assumptions)

--- data/t5/analyze_t5_packing.py
+++ data/t5/analyze_t5_packing.py
@@ -4,13 +4,14 @@ import pandas as pd
 from data.t5.t5_tfds import like_mtf
 
 
+def density(x):
+    return np.count_nonzero(x) / np.prod(x.shape)
+
+
 def analyze_packing(mixture_or_task_name, sequence_length, dataset_split="train"):
     packed_ds = like_mtf(mixture_or_task_name=mixture_or_task_name, sequence_length=sequence_length,
                          dataset_split=dataset_split, pack=True)
     ds = packed_ds
-
-    def density(x):
-        return np.count_nonzero(x) / np.prod(x.shape)
 
     def create_record(packed_example):
         x = packed_example
@@ -27,6 +28,25 @@ def analyze_packing(mixture_or_task_name, sequence_length, dataset_split="train"
     df = pd.DataFrame.from_records([create_record(x) for x in ds.as_numpy_iterator()])
     # df.describe()
     return df
+
+
+def analyze_padding(mixture_or_task_name, sequence_length, dataset_split="train"):
+    padded_ds = like_mtf(mixture_or_task_name=mixture_or_task_name, sequence_length=sequence_length,
+                         dataset_split=dataset_split, pack=False)
+    ds = padded_ds
+
+    def create_record(padded_example):
+        x = padded_example
+        return {
+            "input_seq_length": np.count_nonzero(['inputs']),
+            "target_seq_length": np.count_nonzero(['inputs']),
+            "npacked": 1,
+            "target_density": density(x['targets']),
+            "input_density": density(x['inputs']),
+        }
+
+    df = pd.DataFrame.from_records([create_record(x) for x in ds.as_numpy_iterator()])
+    return ds
 
 
 if __name__ == '__main__':
