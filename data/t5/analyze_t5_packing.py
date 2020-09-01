84e3e37d46e1b1251ef2eead92d8b22a7ee68552
saareliad <saareliad@campus.technion.ac.il>
2020-09-01 12:26:19 +0300

    analysis for all datasets
    small fix packing by 1

--- data/t5/analyze_t5_packing.py
+++ data/t5/analyze_t5_packing.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-from data.t5.t5_tfds import like_mtf
+from data.t5.t5_tfds import like_mtf, get_t5_available_tasks
 
 
 def density(x):
@@ -15,9 +15,10 @@ def analyze_packing(mixture_or_task_name, sequence_length, dataset_split="train"
 
     def create_record(packed_example):
         x = packed_example
+        # Note: seq len is "max in pack"
         return {
-            "input_seq_length": x['inputs_position'].max(),
-            "target_seq_length": x['targets_position'].max(),
+            "input_seq_length": x['inputs_position'].max() + 1,
+            "target_seq_length": x['targets_position'].max() + 1,
             "npacked": x['targets_segmentation'].max(),
             "target_density": density(x['targets']),
             "input_density": density(x['inputs']),
@@ -47,6 +48,37 @@ def analyze_padding(mixture_or_task_name, sequence_length, dataset_split="train"
 
     df = pd.DataFrame.from_records([create_record(x) for x in ds.as_numpy_iterator()])
     return df
+
+
+def infer_no_truncation_padding_seq_length(df):
+    sequence_length = {
+        "inputs": df['input_seq_length'].max(),
+        "targets": df['target_seq_length'].max()
+    }
+    return sequence_length
+
+
+def infer_no_truncation_padding_seq_length_all_splits(mixture_or_task_name, sequence_length,
+                                                      splits=["train", "validation"]):
+    df = pd.concat([analyze_packing(mixture_or_task_name=mixture_or_task_name, sequence_length=sequence_length,
+                                    dataset_split=dataset_split) for dataset_split in splits])
+    return infer_no_truncation_padding_seq_length(df)
+
+
+def infer_no_truncation_padding_seq_length_for_all_t5_available_tasks():
+    names = get_t5_available_tasks(verbose=False)
+    # default seq_len
+    sequence_length = {
+        "inputs": 512,
+        "targets": 512
+    }
+    splits = ["train", "validation"]
+    res = {}
+    for mixture_or_task_name in names:
+        req = infer_no_truncation_padding_seq_length_all_splits(mixture_or_task_name, sequence_length, splits=splits)
+        res[mixture_or_task_name] = req
+
+    return res
 
 
 if __name__ == '__main__':
