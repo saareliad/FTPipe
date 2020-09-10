72a660794ada731804acc764ddca0993f303737b
saareliad <saareliad@campus.technion.ac.il>
2020-09-10 14:49:43 +0300

    fix

--- train.py
+++ train.py
@@ -8,8 +8,8 @@ from enum import Enum, auto
 
 
 class SmallerLastBatchPolicy(Enum):
-    ProportionalStep: auto
-    DropReminder: auto
+    ProportionalStep: auto()
+    DropReminder: auto()
 
 SMALLER_LAST_BATCH_POLICY = SmallerLastBatchPolicy.ProportionalStep
 
