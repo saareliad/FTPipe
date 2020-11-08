81832186d635ddd88631159a0dc63bc9244ee2c1
saareliad <saareliad@campus.technion.ac.il>
2020-11-08 12:22:58 +0200

    WIP: vision transformer hyper parameters.
    Factory for sizes (imagenet)

--- pipe/data/vit.py
+++ pipe/data/vit.py
@@ -59,11 +59,9 @@ def get_transformations(mean, std, resize_size, crop_size, mode='train', jit_scr
     return transform
 
 
-def cifar10_384_transformations(jit_script=False):
+def cifar10_transformations(jit_script=False, resize_size=384, crop_size=384):
     mean = np.array([0.49139968, 0.48215841, 0.44653091])
     std = np.array([0.24703223, 0.24348513, 0.26158784])
-    resize_size = 384
-    crop_size = 384
     train_transform = get_transformations(mean=mean, std=std, crop_size=crop_size, resize_size=resize_size,
                                           mode='train', jit_script=jit_script)
     test_transform = get_transformations(mean=mean, std=std, crop_size=crop_size, resize_size=resize_size, mode='test',
@@ -71,11 +69,9 @@ def cifar10_384_transformations(jit_script=False):
     return train_transform, test_transform
 
 
-def cifar100_384_transformations(jit_script=False):
+def cifar100_transformations(jit_script=False, resize_size=384, crop_size=384):
     mean = np.array([0.5071, 0.4867, 0.4408])
     std = np.array([0.2675, 0.2565, 0.2761])
-    resize_size = 384
-    crop_size = 384
     train_transform = get_transformations(mean=mean, std=std, crop_size=crop_size, resize_size=resize_size,
                                           mode='train', jit_script=jit_script)
     test_transform = get_transformations(mean=mean, std=std, crop_size=crop_size, resize_size=resize_size, mode='test',
@@ -83,11 +79,9 @@ def cifar100_384_transformations(jit_script=False):
     return train_transform, test_transform
 
 
-def imagenet_384_transformations(jit_script=False):
+def imagenet_transformations(jit_script=False, resize_size=384, crop_size=384):
     mean = [0.485, 0.456, 0.406]
     std = [0.229, 0.224, 0.225]
-    resize_size = 384
-    crop_size = 384
     train_transform = get_transformations(mean=mean, std=std, crop_size=crop_size, resize_size=resize_size,
                                           mode='train', jit_script=jit_script)
     test_transform = get_transformations(mean=mean, std=std, crop_size=crop_size, resize_size=resize_size, mode='test',
@@ -95,22 +89,25 @@ def imagenet_384_transformations(jit_script=False):
     return train_transform, test_transform
 
 
-class SepImagenet384DatasetHandler(CommonDatasetHandler):
-    def __init__(self, **kw):
-        super().__init__()
+def sep_imagenet_handler_factory(resize_size=384, crop_size=384):
+    class SepImagenetAutoGenDatasetHandler(CommonDatasetHandler):
+        def __init__(self, **kw):
+            super().__init__()
 
-    def get_train_ds(self, **kw):
-        train_transform, _ = imagenet_384_transformations()
-        return get_imagenet_just_x_or_y_ds(transform=train_transform, **kw)
+        def get_train_ds(self, **kw):
+            train_transform, _ = imagenet_transformations(resize_size=resize_size, crop_size=crop_size)
+            return get_imagenet_just_x_or_y_ds(transform=train_transform, **kw)
 
-    def get_test_ds(self, **kw):
-        # For convenience it is given as test, its actually validation
-        _, test_transform = imagenet_384_transformations()
-        return get_imagenet_just_x_or_y_ds(transform=test_transform, **kw)
+        def get_test_ds(self, **kw):
+            # For convenience it is given as test, its actually validation
+            _, test_transform = imagenet_transformations(resize_size=resize_size, crop_size=crop_size)
+            return get_imagenet_just_x_or_y_ds(transform=test_transform, **kw)
 
-    def get_validation_ds(self, **kw):
-        # For convenience it is given as test, its actually validation
-        NotImplementedError()
+        def get_validation_ds(self, **kw):
+            # For convenience it is given as test, its actually validation
+            NotImplementedError()
+
+    return SepImagenetAutoGenDatasetHandler
 
 
 class SepCifar10_384_DatasetHandler(CommonDatasetHandler):
@@ -118,11 +115,11 @@ class SepCifar10_384_DatasetHandler(CommonDatasetHandler):
         super().__init__()
 
     def get_train_ds(self, **kw):
-        train_transform, _ = cifar10_384_transformations()
+        train_transform, _ = cifar10_transformations(resize_size=384, crop_size=384)
         return get_cifar_10_just_x_or_y_ds(transform=train_transform, train=True, **kw)
 
     def get_test_ds(self, **kw):
-        _, test_transform = cifar10_384_transformations()
+        _, test_transform = cifar10_transformations(resize_size=384, crop_size=384)
         return get_cifar_10_just_x_or_y_ds(transform=test_transform, train=False, **kw)
 
     def get_validation_ds(self, **kw):
@@ -134,19 +131,22 @@ class SepCifar100_384_DatasetHandler(CommonDatasetHandler):
         super().__init__()
 
     def get_train_ds(self, **kw):
-        train_transform, _ = cifar100_384_transformations()
+        train_transform, _ = cifar100_transformations(resize_size=384, crop_size=384)
         return get_cifar_100_just_x_or_y_ds(transform=train_transform, train=True, **kw)
 
     def get_test_ds(self, **kw):
-        _, test_transform = cifar100_384_transformations()
+        _, test_transform = cifar100_transformations(resize_size=384, crop_size=384)
         return get_cifar_100_just_x_or_y_ds(transform=test_transform, train=False, **kw)
 
     def get_validation_ds(self, **kw):
         NotImplementedError()
+
 
 # For ImageNet results in Table 2, we fine-tuned at higher resolution:
 # 512 for ViT-L/16 and 518 for ViT-H/14
 
 register_dataset("cifar10_384", SepCifar10_384_DatasetHandler)
 register_dataset("cifar100_384", SepCifar100_384_DatasetHandler)
-register_dataset("imagenet_384", SepImagenet384DatasetHandler)
+register_dataset("imagenet_384", sep_imagenet_handler_factory(resize_size=384, crop_size=384))
+register_dataset("imagenet_512", sep_imagenet_handler_factory(resize_size=512, crop_size=512))
+register_dataset("imagenet_518", sep_imagenet_handler_factory(resize_size=518, crop_size=518))
