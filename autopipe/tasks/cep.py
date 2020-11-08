257ccc540bd0ab50b0bf17dce4aedb155b58a143
saareliad <saareliad@campus.technion.ac.il>
2020-11-08 14:42:13 +0200

    CEP model for Najeeb (helping group). Also simple add-model example for partitioning.

---
+++ autopipe/tasks/cep.py
@@ -0,0 +1,84 @@
+import torch
+from torch.utils.data import DataLoader
+
+from autopipe.tasks import Parser, PartitioningTask, register_task
+from models.normal.cep import Net, Dataset
+
+
+# # N = number of nodes in graph, K = clique size
+# # C = constant as defined in the paper, samples_num = arbitrary high number
+# N, K, C, samples_num = 361, 18, 20000, 1e11
+# # Loss, Optimizers etc..
+# loss_func = nn.BCEWithLogitsLoss()
+#
+# model = Net(N, C)
+# dataset = Dataset(N, K, samples_num)
+# optimizer = torch.optim.AdamW(model.parameters(), lr=0.001)
+#
+# print(summary(model, input_size=(N * (N - 1) // 2,)))
+
+
+class CEPParser(Parser):
+    def _add_model_args(self, group):
+        group.add_argument("--N", type=int, default=361)
+        group.add_argument("--C", type=int, default=20000)
+
+    def _add_data_args(self, group):
+        group.add_argument("--K", type=int, default=18)
+        group.add_argument("--samples_num", type=int, default=1e11)
+
+    def _default_values(self):
+        return {
+            "n_iter": 10,
+            "n_partitions": 4,
+            "bw": 12,
+            "partitioning_batch_size": 1,
+            "analysis_batch_size": 1,
+
+            # "basic_blocks": ["T5Attention"]
+        }
+
+    def _auto_file_name(self, args) -> str:
+        bw_str = str(args.bw).replace(".", "_")
+        model_str = str("cep_net").replace("-", "_")
+
+        model_str += f"N{args.N}_C{args.C}"
+        output_file = f"{model_str}_{args.n_partitions}p_bw{bw_str}"
+
+        if args.async_pipeline:
+            output_file += "_async"
+
+        m = args.partitioning_method.lower()
+        tmp = m if m != "2dbin" else "virtual_stages"
+        output_file += f"_{tmp}"
+
+        return output_file
+
+
+class CEPPartitioningTask(PartitioningTask):
+    def __init__(self, args) -> None:
+        super().__init__(args)
+
+    @property
+    def batch_dim(self) -> int:
+        return 0
+
+    def register_functions(self):
+        pass
+
+    #     register_new_explicit_untraced_function(operator.is_, operator)
+    #     register_new_explicit_untraced_function(operator.is_not, operator)
+    #     register_new_traced_function(math.log, math)
+    #     register_new_traced_function(torch.einsum, torch)
+
+    def get_model(self, args) -> torch.nn.Module:
+        return Net(n=args.N, c=args.C)
+
+    def get_input(self, args, analysis=False):
+        dataset = Dataset(n=args.N, k=args.K, max_samples_num=args.samples_num)
+        batch_size = args.analysis_batch_size if analysis else args.partitioning_batch_size
+        loader = DataLoader(dataset, batch_size=batch_size)
+        return next(iter(loader))
+
+
+register_task("cep", CEPParser, CEPPartitioningTask)
---
+++ models/normal/cep.py
@@ -0,0 +1,80 @@
+from itertools import combinations
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+# Dataset for generating graphs on the fly
+class Dataset(torch.utils.data.Dataset):
+
+    def __init__(self, n, k, max_samples_num):
+        'Initialization'
+        self.samples_num = max_samples_num
+        self.n = n
+        self.node_list = list(range(n))
+        self.k = k
+        self.edge_dict = {}
+
+        A, B = np.tril_indices(n, k=-1)
+        ctr = 0
+        for i, j in zip(A, B):
+            self.edge_dict[(i, j)] = ctr
+            self.edge_dict[(j, i)] = ctr
+            ctr += 1
+
+    def __len__(self):
+        return self.samples_num
+
+    def __getitem__(self, index):
+        X = torch.randint(0, 2, (self.n * (self.n - 1) // 2,)) - 0.5
+
+        if index % 2 == 0:
+            ch_nodes = np.random.choice(self.node_list, self.k, replace=False)
+            X[[self.edge_dict[c] for c in combinations(ch_nodes, 2)]] = 0.5
+            return X, torch.tensor([1.0])
+
+        return X, torch.tensor([0.0])
+
+
+class Net(nn.Module):
+
+    def __init__(self, n, c):
+        super(Net, self).__init__()
+
+        self.input_layer = nn.Linear(n * (n - 1) // 2, 3 * n * (n - 1) // 4)
+        self.bn1 = nn.BatchNorm1d(3 * n * (n - 1) // 4)
+        self.h1_layer = nn.Linear(3 * n * (n - 1) // 4, c)
+        self.bn2 = nn.BatchNorm1d(c)
+        self.h2_layer = nn.Linear(c, c // 20)
+        self.bn3 = nn.BatchNorm1d(c // 20)
+        self.output_layer = nn.Linear(c // 20, 1)
+
+    def forward(self, x):
+        x = F.leaky_relu(self.bn1(self.input_layer(x)))
+        x = F.leaky_relu(self.bn1(self.h1_layer(x)))
+        x = F.leaky_relu(self.bn1(self.h2_layer(x)))
+        x = self.output_layer(x)
+        return x
+
+
+if __name__ == '__main__':
+    # N = number of nodes in graph, K = clique size
+    # C = constant as defined in the paper, samples_num = arbitrary high number
+    N, K, C, samples_num = 361, 18, 20000, 1e11
+    # Loss, Optimizers etc..
+    loss_func = nn.BCEWithLogitsLoss()
+
+    model = Net(N, C)
+    dataset = Dataset(N, K, samples_num)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=0.001)
+
+    print(f"Num parameters: {sum(p.numel() for p in model.parameters())}")
+
+    try:
+        from torchsummary import summary
+
+        print(summary(model, input_size=(N * (N - 1) // 2,)))
+    except ImportError as e:
+        print("please install torchsummary for additional information")
