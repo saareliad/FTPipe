9f8d720c946934decd891ba81dfe74387a7474f0
saareliad <saareliad@campus.technion.ac.il>
2020-11-10 21:33:40 +0200

    work on cep
    register model instance

--- pipe/models/registery/cep.py
+++ pipe/models/registery/cep.py
@@ -1,3 +1,5 @@
+import functools
+
 from models.normal.cep import Net
 from pipe.models.registery.model_handler import CommonModelHandler, register_model
 
@@ -8,7 +10,7 @@ def register_cep_model(n=50, k=11, c=500, n_split=4):
 
 
 class CEPModelHandler(CommonModelHandler):
-    def __init__(self, normal_model_fn=register_cep_model, *args, **kw):
+    def __init__(self, normal_model_fn, *args, **kw):
         super().__init__(*args, **kw)
         self.normal_model_fn = normal_model_fn
 
@@ -18,4 +20,6 @@ class CEPModelHandler(CommonModelHandler):
         return self.normal_model_instance
 
 
-register_model(name="cep_netN50_C500_4p_bw12_metis", handler=CEPModelHandler(normal_model_fn=Net(n=50, c=500, n_split=4)))
+register_model(name="cep_netN50_C500_4p_bw12_metis",
+               handler=CEPModelHandler(normal_model_fn=functools.partial(register_cep_model, n=50, c=500, n_split=4),
+                                       generated_file_name_or_path="cep_netN50_C500_4p_bw12_metis"))
