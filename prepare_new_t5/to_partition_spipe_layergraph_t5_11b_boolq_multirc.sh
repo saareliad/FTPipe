5154735be2c4507e0561010bf2a04d5ec25dc01b
Saar Eliad <saareliad@campus.technion.ac.il>
2021-05-27 23:59:26 +0300

    gpipe and partition script

---
+++ prepare_new_t5/to_partition_spipe_layergraph_t5_11b_boolq_multirc.sh
@@ -0,0 +1,40 @@
+#rm new_trace_cache_t511b_512_4_lg new_prof_cache_t511b_512_4_lg_ftpipe
+python -m autopipe.partition new_t5 \
+ --model_name_or_path \
+ t5-11b \
+ --t5_task \
+ squad1 \
+ --lmhead \
+ --n_iter \
+ 2 \
+ --analysis_batch_size \
+ 1 \
+ --partitioning_batch_size \
+ 1 \
+ --ct \
+ new_trace_cache_t511b_512_4_lg \
+ --cp \
+ new_prof_cache_t511b_512_4_lg_ftpipe \
+ --stateless_tied \
+ --lmhead \
+ --n_partitions \
+ 4 \
+ --max_seq_length \
+ 512 \
+ --answer_max_seq_length \
+ 4 \
+ --partitioning_method \
+  pipedream \
+ --preset \
+  ftpipe \
+ --dont_use_async_meta_alg \
+ --save_memory_mode \
+ --disable_op_profiling \
+ --special_blocks \
+ T5Block \
+ --basic_blocks \
+ T5Block \
+ --output_file \
+ layer_graph_ 
+ # > partitioning_output_mpipe_t53b_512_4_lg_ftpipe.txt
+
--- prepare_new_t5/to_run_t511b.sh
+++ prepare_new_t5/to_run_t511b.sh
@@ -1,7 +1,18 @@
 export OMP_NUM_THREADS=2
-mkdir /home_local/saareliad/data/moved_cache
-nvidia-cuda-mps-control -d # Start deamon in background
 
+#mkdir /home_local/saareliad/data/moved_cache
+nvidia-cuda-mps-control -d # Start deamon in background
+python -m pipe.main --config pipe/configs/t5/t511b/boolq/gpipe_layer_graph.json --seed 42 --mode preproc
+mpirun -np 7 python -m pipe.main --config pipe/configs/t5/t511b/boolq/gpipe_layer_graph.json --seed 42
+
+exit 0
+nvidia-cuda-mps-control -d # Start deamon in background
 python -m pipe.main --config pipe/configs/t5/t511b/boolq/stale_layer_graph.json --seed 42 --mode preproc
 mpirun -np 7 python -m pipe.main --config pipe/configs/t5/t511b/boolq/stale_layer_graph.json --seed 42
 
+
+#### spipe
+# export OMP_NUM_THREADS=4
+# bash prepare_new_t5/to_partition_spipe_layergraph_t5_11b_boolq_multirc.sh 
+# python -m pipe.main --config pipe/configs/t5/t511b/boolq/stale_layer_graph_pipedream.json --seed 42 --mode preproc
+# mpirun -np 4 python -m pipe.main --config pipe/configs/t5/t511b/boolq/stale_layer_graph_pipedream.json --seed 42
